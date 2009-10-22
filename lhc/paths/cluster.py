b1fdd238c6f17b51d2ea6dc3bbeb14bcc97a337a
Liam Childs <childs@grey.bioinformatics.up>
2009-10-22 11:10:25 +0200

    Committed minor changes

--- lhc/paths/cluster.py
+++ lhc/paths/cluster.py
@@ -1,1 +1,9 @@
-jobdir = '/home/childs/jobdir'
+jobdir = '/data/home/childs/jobdir'
+logdir = '/data/home/childs/logdir'
+
+qsub = '/usr/bin/qsub'
+qstat = '/usr/bin/qstat'
+qdel = '/usr/bin/qdel'
+
+pbs_slave = '/data/home/childs/scripts/pbs/PbsSlave.py'
+mpi_slave = '/data/home/childs/scripts/mpi/slave.py'
