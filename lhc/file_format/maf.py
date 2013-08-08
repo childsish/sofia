14f4543d06a3883775193a1d96b4ac40f936bc1b
Liam Childs <liam_childs@hotmail.com>
2013-08-08 22:17:01 +0200

    Updated maf and vcf file format parsers

--- lhc/file_format/maf.py
+++ lhc/file_format/maf.py
@@ -1,4 +1,29 @@
-from functools import partial
-from table import iterTable
+from csv import iterCsv, ColumnBuilder, FieldBuilder
+from lhc.binf.genomic_interval import interval
 
-iterMaf = partial(iterTable, typ='maf')
+def iterMaf(fname):
+    column_builder = ColumnBuilder()
+    column_builder.registerColumn('chr', 4)
+    column_builder.registerColumn('fr', 5, int)
+    column_builder.registerColumn('to', 6, int)
+    column_builder.registerColumn('strand', 7)
+    column_builder.registerColumn('type', 9)
+    column_builder.registerColumn('ref', 10)
+    column_builder.registerColumn('allele1', 11)
+    column_builder.registerColumn('allele2', 12)
+    column_builder.registerColumn('genotype_id', 15)
+    
+    field_builder = FieldBuilder()
+    field_builder.registerField('ivl', parseInterval)
+    field_builder.registerField('type')
+    field_builder.registerField('ref')
+    field_builder.registerField('allele1')
+    field_builder.registerField('allele2')
+    field_builder.registerField('genotype_id')
+    
+    for fields in iterCsv(fname, column_builder, field_builder, skip=1):
+        yield fields
+
+def parseInterval(cols):
+    return interval(cols.chr, cols.fr - 1, cols.to, cols.strand)
+
--- lhc/file_format/vcf.py
+++ lhc/file_format/vcf.py
@@ -1,4 +1,27 @@
-from functools import partial
-from table import iterTable
+from csv import iterCsv, ColumnBuilder, FieldBuilder
+from lhc.binf.genomic_interval import interval
 
-iterVcf = functool.partial(iterTable, type='vcf')
+def iterVcf(fname):
+    column_builder = ColumnBuilder()
+    column_builder.registerColumn('chr', 0)
+    column_builder.registerColumn('pos', 1, int)
+    column_builder.registerColumn('ref', 3)
+    column_builder.registerColumn('alt', 4)
+    column_builder.registerColumn('qual', 5, float)
+    column_builder.registerColumn('attr', 6)
+    
+    field_builder = FieldBuilder()
+    field_builder.registerField('ivl', parseInterval)
+    field_builder.registerField('ref')
+    field_builder.registerField('alt')
+    field_builder.registerField('qual')
+    field_builder.registerField('attr', parseAttributes)
+    
+    for fields in iterCsv(fname, column_builder, field_builder, skip=1):
+        yield fields
+
+def parseInterval(cols):
+    return interval(cols.chr, cols.pos, cols.pos)
+
+def parseAttributes(cols):
+    return dict(part.strip().split('=', 1) for part in cols.strip().split(';'))
