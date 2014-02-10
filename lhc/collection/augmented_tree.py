20c3acb31075fa348f8581c367879334e8a6e046
Liam Childs <liam_childs@hotmail.com>
2014-02-10 09:07:12 +0100

    Unrolled augmented tree intersect recursion

--- lhc/collection/augmented_tree.py
+++ lhc/collection/augmented_tree.py
@@ -8,48 +8,7 @@ class AugmentedTree(object):
     def __init__(self, ivls):
         self.ivls = sorted(ivls, key=operator.attrgetter('start'))
         self.stops = [0 for ivl in ivls]
-        self.refresh()
-    
-    def refresh(self, lo=None, hi=None):
-        lo = 0 if lo is None else lo
-        hi = len(self.ivls) if hi is None else hi
-        mid = (hi + lo) / 2
-        
-        if lo == mid:
-            stop = self.ivls[mid].stop
-        elif mid + 1 == hi:
-            stop = max(self.refresh(lo, mid), self.ivls[mid].stop)
-        else:
-            stop = max(self.refresh(lo, mid), self.refresh(mid + 1, hi))
-        self.stops[mid] = stop
-        return stop
-    
-    def intersect(self, qry, lo=None, hi=None):
-        lo = 0 if lo is None else lo
-        hi = len(self.ivls) if hi is None else hi
-        mid = (hi + lo) / 2
-        
-        ivl = self.ivls[mid]
-        stop = self.stops[mid]
-        
-        print lo, mid, hi, ivl, stop
-        
-        left = lo != mid and qry.start < stop
-        right = mid + 1 < hi and ivl.start < qry.stop
-        
-        res = []
-        if left:
-            print 'left'
-            res.extend(self.intersect(qry, lo, mid))
-        print qry, ivl, slicetools.overlaps(qry, ivl)
-        if slicetools.overlaps(qry, ivl):
-            print 'add'
-            res.append(ivl)
-        if right:
-            print 'right'
-            res.extend(self.intersect(qry, mid + 1, hi))
-        print 'up'
-        return res
+        self._refresh()
     
     def __iter__(self):
         return iter(self.ivls)
@@ -61,3 +20,44 @@ class AugmentedTree(object):
     def __setstate__(self, state):
         for k, v in state.iteritems():
             setattr(self, k, v)
+    
+    def _refresh(self, lo=None, hi=None):
+        """Refresh the maximum stop value for of the subtree at all nodes.
+        
+        Keyword variables:
+        lo -- lower bound of the array/tree
+        hi -- upper bound of the array/tree
+        """
+        lo = 0 if lo is None else lo
+        hi = len(self.ivls) if hi is None else hi
+        mid = (hi + lo) / 2
+        
+        if lo == mid:
+            stop = self.ivls[mid].stop
+        elif mid + 1 == hi:
+            stop = max(self._refresh(lo, mid), self.ivls[mid].stop)
+        else:
+            stop = max(self._refresh(lo, mid), self._refresh(mid + 1, hi))
+        self.stops[mid] = stop
+        return stop
+    
+    def intersect(self, qry):
+        """Find all intervals intersecting the given interval
+        
+        Keyword arguments:
+        qry -- query interval
+        """
+        res = []
+        stk = [(0, len(self.ivls))]
+        while len(stk) > 0:
+            lo, hi = stk.pop()
+            mid = (hi + lo) / 2
+            ivl = self.ivls[mid]
+            stop = self.stops[mid]
+            if slicetools.overlaps(qry, ivl):
+                res.append(ivl)
+            if lo != mid and qry.start < stop:
+                stk.append((lo, mid))
+            if mid + 1 < hi and ivl.start < qry.stop:
+                stk.append((mid + 1, hi))
+        return res
--- lhc/test/collection/augmented_tree.py
+++ lhc/test/collection/augmented_tree.py
@@ -12,7 +12,8 @@ class TestAugmentedTree(unittest.TestCase):
     def test_intersect(self):
         ivls = [slice(0, 10), slice(2, 30), slice(3, 20), slice(5, 7), slice(9, 15), slice(20, 25), slice(21, 22), slice(25, 31), slice(35, 39), slice(36, 39)]
         tree = AugmentedTree(ivls)
-        print tree.intersect(slice(15, 30))
+        self.assertEquals(set(map(str, tree.intersect(slice(15, 30)))),
+            set(map(str, [slice(2, 30, None), slice(3, 20, None), slice(20, 25, None), slice(21, 22, None), slice(25, 31, None)])))
 
 if __name__ == '__main__':
     unittest.main()
