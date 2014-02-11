43ea611dfe872efb951b956dc0a9dade39368e22
Liam Childs <liam_childs@hotmail.com>
2014-02-11 09:43:58 +0100

    Finished nested_containment_list and tests

--- lhc/collection/nested_containment_list.py
+++ lhc/collection/nested_containment_list.py
@@ -1,82 +1,87 @@
 import numpy as np
 
+from bisect import bisect_left
 from collections import Counter
 from netCDF4 import Dataset
+from tempfile import mkstemp
 
 class NestedContainmentList(object):
-
-    def __init__(self, root, mode='r', diskless=False):
-        #self.root = root
-        #if isinstance(root, basestring):
-        #    self.root = Dataset(root, mode, diskless=diskless)
-        pass
+    def __init__(self, root, ivls=None):
+        mode = 'r' if ivls is None else 'w'
+        self.root = Dataset(root, mode) if isinstance(root, basestring) else root
+        self.closed = False
+        if mode == 'w':
+            parent_ids = self._getParentIds(ivls)
+            grp_table, pnt2grp = self._getGroupTable(parent_ids)
+            ivl_table = self._getIntervalTable(ivls, pnt2grp, parent_ids)
+            self._populateRoot(ivl_table, grp_table)
     
-    def __getitem__(self, key):
-        if isinstance(key, int):
-            ivl = self.root.variables['ivls'][key,:]
-            return slice(ivl[0], -ivl[1])
-    
-    def insertIntervals(self, ivls):
-        ivls = np.array([[ivl.start, -ivl.stop] for ivl in ivls], dtype='i4')
-        ivl_table, grp_table, ivl_map = self.getIntervalTable(ivls)
-        self.populateRoot(ivl_table, grp_table)
-        return ivl_map
-    
-    def getIntervalTable(self, ivls):
-        def contains(a, b):
-            return a[0] <= b[0] and a[1] <= b[1]
+    def intersect(self, ivl):
+        grp_var = self.root.variables['grps']
+        ivl_var = self.root.variables['ivls']
+        res = []
         
-        owner_ids = np.zeros(len(ivls), dtype='i4')
-        member_ids = np.zeros(len(ivls), dtype='i4')
+        stk = [0]
+        while len(stk) > 0:
+            grp_fr, grp_sz = grp_var[stk.pop(0), :]
+            grp_to = grp_fr + grp_sz
+            grp_to = grp_fr + bisect_left(ivl_var[grp_fr:grp_to,0], ivl.stop)
+            while grp_fr < grp_to:
+                start, stop, grp_idx = ivl_var[grp_fr]
+                if ivl.start < stop:
+                    res.append(slice(start, stop))
+                if grp_idx != -1:
+                    stk.append(grp_idx)
+                grp_fr += 1
+        return res
+    
+    def close(self):
+        if hasattr(self, 'closed') and not self.closed and hasattr(self.root, 'closed'):
+            self.root.close()
+    
+    def _getParentIds(self, ivls):
+        parent_ids = -1 * np.ones(len(ivls), dtype='i4')
         
-        idxs = np.lexsort((ivls[:,1], ivls[:,0]))
-        sublist_id = 1
-        stk = []
-        for i in xrange(len(idxs) - 1):
-            member_ids[i] = 0 if len(stk) == 0 else stk[-1][1]
-            if contains(ivls[idxs[i]], ivls[idxs[i + 1]]):
-                stk.append((ivls[idxs[i]], sublist_id))
-                owner_ids[idxs[i]] = sublist_id
-                sublist_id += 1
-            while len(stk) > 0 and not contains(stk[-1][0], ivls[idxs[i + 1]]):
-                stk.pop()
-        member_ids[-1] = 0 if len(stk) == 0 else stk[-1][1]
-        idxs = idxs[np.argsort(member_ids)]
-        
-        grp_table = self.getSubGroupTable(member_ids)
-        return np.column_stack((ivls[idxs], owner_ids[idxs])), grp_table, np.argsort(idxs)
-    
-    def getSubGroupTable(self, member_ids):
-        cnts = Counter(member_ids)
-        grp_table = np.empty((len(cnts), 2), dtype='i4')
-        ttl = 0
-        for k, cnt in sorted(cnts.iteritems()):
-            grp_table[k,] = np.array([ttl, cnt]) 
-            ttl += cnt
-        return grp_table
-    
-    def populateRoot(self, ivls, grps):
-        self.root.createDimension('ivls', len(ivls))
-        self.root.createDimension('ivl_col', 3)
-        self.root.createDimension('grps', len(grps))
-        self.root.createDimension('grp_col', 2)
-        self.root.createVariable('ivls', 'i4', ('ivls', 'ivl_col'))[:] = ivls
-        self.root.createVariable('grps', 'i4', ('grps', 'grp_col'))[:] = grps
-    
-    def _build(self, ivls):
-        nsub = 0
-        sublist_ids = len(ivls) * [-1]
         ivls.sort()
         i = 0
         while i < len(ivls):
             parent = i
-            i = parent + 1
-            while i < len(ivls) and parent > 0:
+            i += 1
+            while i < len(ivls) and parent >= 0:
                 if ivls[i].stop > ivls[parent].stop or ivls[i] == ivls[parent]:
-                    parent = sublist_ids[parent]
+                    parent = parent_ids[parent]
                 else:
-                    sublist_ids[i] = parent
-                    nsub += 1
+                    parent_ids[i] = parent
                     parent = i
                     i += 1
-        print sublist_ids
+        return parent_ids
+    
+    def _getGroupTable(self, parent_ids):
+        cnts = Counter(parent_ids)
+        pnt2grp = {}
+        grp_table = np.empty((len(cnts), 2), dtype='i4')
+        
+        cpos = 0
+        for group_id, (parent_id, group_size) in enumerate(sorted(cnts.iteritems())):
+            pnt2grp[parent_id] = group_id
+            grp_table[group_id,0] = cpos
+            grp_table[group_id,1] = group_size
+            cpos += group_size
+        del pnt2grp[-1]
+        return grp_table, pnt2grp
+    
+    def _getIntervalTable(self, ivls, pnt2grp, parent_ids):
+        ivl_table = np.array([[ivl.start, ivl.stop, -1] for ivl in ivls], dtype='i4')
+        for pnt_id, grp_id in pnt2grp.iteritems():
+            ivl_table[pnt_id,2] = grp_id
+        idxs = np.argsort(parent_ids)
+        ivl_table = ivl_table[idxs]
+        return ivl_table
+    
+    def _populateRoot(self, ivl_table, grp_table):
+        self.root.createDimension('ivls', len(ivl_table))
+        self.root.createDimension('ivl_col', 3)
+        self.root.createDimension('grps', len(grp_table))
+        self.root.createDimension('grp_col', 2)
+        self.root.createVariable('ivls', 'i4', ('ivls', 'ivl_col'))[:] = ivl_table
+        self.root.createVariable('grps', 'i4', ('grps', 'grp_col'))[:] = grp_table
--- lhc/test/collection/nested_containment_list.py
+++ lhc/test/collection/nested_containment_list.py
@@ -1,13 +1,32 @@
+import os
 import unittest
+import numpy as np
 
+from tempfile import mkstemp
 from lhc.collection.nested_containment_list import NestedContainmentList as NCList
 
 class Test(unittest.TestCase):
+    def setUp(self):
+        self.hndl, self.fname = mkstemp()
+        os.close(self.hndl)
+    
     def test_init(self):
         ivls = [slice(0, 10), slice(2, 30), slice(3, 20), slice(5, 7), slice(9, 15), slice(20, 25), slice(21, 22), slice(25, 31), slice(35, 39), slice(36, 39)]
         
-        nclist = NCList(None)
-        nclist._build(ivls)
+        nclist = NCList(self.fname, ivls)
+        
+        self.assertEquals(nclist.root.variables['ivls'][:].tolist(), [[0, 10, -1], [2, 30, 1], [25, 31, -1], [35, 39, 4], [3, 20, 2], [20, 25, 3], [5, 7, -1], [9, 15, -1], [21, 22, -1], [36, 39, -1]])
+        self.assertEquals(nclist.root.variables['grps'][:].tolist(), [[0, 4], [4, 2], [6, 2], [8, 1], [9, 1]])
+        nclist.close()
+    
+    def test_intersect(self):
+        ivls = [slice(0, 10), slice(2, 30), slice(3, 20), slice(5, 7), slice(9, 15), slice(20, 25), slice(21, 22), slice(25, 31), slice(35, 39), slice(36, 39)]
+        
+        nclist = NCList(self.fname, ivls)
+        
+        self.assertEquals(set(map(str, nclist.intersect(slice(15, 30)))),
+            set(map(str, [slice(2, 30, None), slice(3, 20, None), slice(20, 25, None), slice(21, 22, None), slice(25, 31, None)])))
+        nclist.close()
 
 if __name__ == "__main__":
     unittest.main()
