67c0bf2614ac20f6920f592bb1ff9f7807c320db
Liam Childs <liam_childs@hotmail.com>
2014-02-14 14:42:06 +0100

    New gene model captures Gene, Transcript and Exon

--- lhc/binf/gene_model.py
+++ lhc/binf/gene_model.py
@@ -1,12 +1,28 @@
-class TranscriptModel(object):
-    def __init__(self, ivls=[]):
-        self.ivls = ivls
-    
-    def getSubSeq(self, seq):
-        return ''.join([ivl.getSubSeq(seq) for ivl in self.ivls])
+from collections import OrderedDict
+from lhc.tools import enum
 
-class GeneModel(object):
-    def __init__(self, name, ivl):
+class Gene(object):
+    def __init__(self, name, ivl, transcripts=None):
         self.name = name
         self.ivl = ivl
-        self.transcripts = {}
+        self.transcripts = OrderedDict() if transcripts is None else transcripts
+
+class Transcript(object):
+    def __init__(self, name, ivl, exons=None):
+        self.name = name
+        self.ivl = ivl
+        self.exons = [] if exons is None else exons
+    
+    def getSubSeq(self, seq, valid_types=set(['CDS', 'UTR5', 'UTR3'])):
+        return ''.join([exon.getSubSeq(seq) for exon in self.exons if exon.type in valid_types])
+
+class Exon(object):
+    
+    TYPE = enum(['gene', 'transcript', 'CDS', 'UTR5', 'UTR3'])
+    
+    def __init__(self, ivl, type_):
+        self.ivl = ivl
+        self.type = type_
+    
+    def getSubSeq(self, seq):
+        return self.ivl.getSubSeq(seq)
