b13d7ee2cc5742bef012bdbf94851403d37840ff
Liam Childs <liam_childs@hotmail.com>
2014-02-19 11:44:00 +0100

    Model set now uses nested containment lists

--- lhc/binf/collection/model_set.py
+++ lhc/binf/collection/model_set.py
@@ -1,207 +1,165 @@
 import sqlite3
+import itertools
 
-from collections import defaultdict
-from lhc.binf.genomic_interval import interval
-
-class Model(object):
-    
-    def __init__(self, name, ivl, type):
-        self.name = name
-        self.ivl = ivl
-        self.type = type
-        self.children = []
+from operator import attrgetter
+from lhc.tools import argsort
+from lhc.binf.genomic_coordinate import Interval
+from lhc.binf.gene_model import Gene, Transcript, Exon
+from lhc.collection.nested_containment_list import getTables
 
 class ModelSet(object):
-    
-    def __init__(self, fname):
+    def __init__(self, fname, mdls=[]):
         self.conn = sqlite3.connect(fname)
-        self.createTables()
+        if len(mdls) > 0:
+            self._createTables()
+            self._insertModels(mdls)
+            self._createIndices()
     
+    def get(self, key, fetch_type=Exon.TYPE.gene):
+        assert fetch_type in (Exon.TYPE.gene, Exon.TYPE.transcript)
+        
+        cur = self.conn.cursor()
+        qry = '''SELECT model.id, model.chromosome, interval.start, interval.stop, model.strand, model.type, model.name, model.parent
+        FROM model AS gene, model, interval
+        WHERE gene.name = ? AND
+            gene.type = "{}" AND
+            model.left >= gene.left AND
+            model.right <= gene.right AND
+            interval.id = model.interval
+        ORDER BY model.id, model.ordering'''.format(fetch_type)
+        
+        res = {}
+        res_id = None
+        for id, chm, start, stop, strand, type_, name, parent in cur.execute(qry, (key,)):
+            if type_ == fetch_type:
+                res_id = id
+            ivl = Interval(chm, start, stop, strand)
+            if type_ == Exon.TYPE.gene:
+                res[id] = Gene(name, ivl)
+            elif type_ == Exon.TYPE.transcript:
+                res[id] = Transcript(name, ivl)
+                res[parent].transcripts[name] = res[id]
+            else:
+                res[id] = Exon(ivl, type_)
+                res[parent].exons.append(res[id])
+        return res[res_id]
+    
+    def intersect(self, ivl, fetch_type=Exon.TYPE.gene):
+        cur = self.conn.cursor()
+        
+        qry = '''
+            WITH RECURSIVE
+                cumulative_intervals(interval, sublist) AS (
+                    SELECT i.id, i.sublist
+                    FROM interval AS i, sublist AS s
+                    WHERE s.id = 1 AND
+                        i.id >= s.interval AND
+                        i.id < s.interval + s.size AND
+                        i.start < ? AND
+                        ? < i.stop
+                    UNION
+                    SELECT i.id, i.sublist
+                    FROM interval AS i, sublist AS s, cumulative_intervals
+                    WHERE s.id = cumulative_intervals.sublist AND
+                        i.id >= s.interval AND
+                        i.id < s.interval + s.size AND
+                        i.start < ? AND
+                        ? < i.stop
+                )
+            SELECT model.name
+            FROM model, cumulative_intervals
+            WHERE model.interval = cumulative_intervals.interval AND
+                model.chromosome = ? AND
+                model.type == ?
+        '''
+        
+        return [self.get(name, fetch_type) for name, in\
+            cur.execute(qry, (ivl.stop, ivl.start, ivl.stop, ivl.start, ivl.chr, fetch_type))]
+
     def __del__(self):
         if hasattr(self, 'conn'):
             self.conn.commit()
             self.conn.close()
     
-    def __getitem__(self, key):
-        if isinstance(key, basestring):
-            return self.getModelById(key)
-        elif isinstance(key, interval):
-            return self.getModelsInInterval(key)
-        raise TypeError('Unknown type used to get gene models: %s'%type(key))
-    
-    def createTables(self):
+    def _createTables(self):
         cur = self.conn.cursor()
-        try:
-            cur.execute('''CREATE VIRTUAL TABLE interval
-                USING rtree(id, ivl_fr, ivl_to);''')
-        except sqlite3.OperationalError:
-            pass
+        cur.execute('''CREATE TABLE interval (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            start INTEGER,
+            stop INTEGER,
+            sublist INTEGER REFERENCES sublist(id) DEFERRABLE INITIALLY DEFERRED)''')
+        cur.execute('''CREATE TABLE sublist (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            interval INTEGER REFERENCES interval(id),
+            size INTEGER)''')
         cur.execute('''CREATE TABLE IF NOT EXISTS model (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            chr TEXT,
-            interval_id REFERENCES interval(id),
-            type TEXT,
+            interval INTEGER REFERENCES interval(id),
+            chromosome TEXT,
             strand TEXT,
-            parent_id,
-            FOREIGN KEY(parent_id) REFERENCES model(id)
-        );''')
-        cur.execute('''CREATE TABLE IF NOT EXISTS identifier (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            model_id REFERENCES model(id),
-            value TEXT,
-            type TEXT
-        );''')
+            type TEXT,
+            name TEXT,
+            ordering INTEGER,
+            parent INTEGER,
+            left INTEGER,
+            right INTEGER,
+            FOREIGN KEY (parent) REFERENCES model(id)
+        )''')
     
-    def addModelSegment(self, ivl, type, parent_id=None):
-        cur = self.conn.cursor()
-        qry = 'INSERT INTO interval VALUES (NULL, ?, ?)'
-        cur.execute(qry, (ivl.start, ivl.stop))
-        interval_id = cur.lastrowid
-        qry = 'INSERT INTO model VALUES (NULL, ?, ? ,?, ?, ?)'
-        cur.execute(qry, (ivl.chr, interval_id, type, ivl.strand, parent_id))
-        model_id = cur.lastrowid
-        return model_id
-    
-    def addIdentifier(self, model_id, value, type='PRIMARY'):
-        cur = self.conn.cursor()
-        qry = 'SELECT id FROM identifier WHERE model_id = :model_id AND type = :type'
-        id = cur.execute(qry, locals()).fetchone()
-        qry = 'INSERT INTO identifier VALUES (NULL, :model_id, :value, :type)' if id is None else\
-            'UPDATE identifier SET value = :value WHERE model_id = :model_id AND type = :type'
-        cur.execute(qry, locals())
-        return cur.lastrowid
-
-    def finaliseIntervals(self):
-        pass
-    
-    def getModelById(self, key):
-        cur = self.conn.cursor()
+    def _insertModels(self, mdls):
+        flat_mdls, ivls = self._flattenModels(mdls)
+        ivl_table, grp_table, idxs = getTables(ivls)
+        for mdl, idx in itertools.izip(flat_mdls, idxs):
+            mdl[0] = idx
         
-        qry = '''SELECT model.id, model.chr, model.type, model.strand,
-                interval.ivl_fr, interval.ivl_to
-            FROM identifier, model, interval
-            WHERE
-                interval.id = model.interval_id AND
-                model.id = identifier.model_id AND
-                identifier.value = ?;
-        '''
-        res_id, chm, typ, strand, fr, to = cur.execute(qry, (key,)).fetchone()
-        models = {}
-        children = defaultdict(list)
-        qry = '''SELECT identifier.value, model.id, model.type, model.strand,
-                model.parent_id, interval.ivl_fr, interval.ivl_to
-            FROM model, interval
-            LEFT JOIN identifier
-            ON
-                identifier.type = 'PRIMARY' AND
-                identifier.model_id = model.id
-            WHERE
-                model.chr = ? AND
-                model.interval_id = interval.id AND
-                interval.ivl_fr >= ? AND
-                interval.ivl_to <= ?;'''
-        rows = cur.execute(qry, (chm, fr, to))
-        for name, id, typ, strand, parent, fr, to in rows:
-            models[id] = Model(name, interval(chm, fr, to, strand), typ)
-            if parent is not None:
-                children[parent].append(id)
-        for parent_id, child_ids in children.iteritems():
-            if parent_id in models:
-                models[parent_id].children =\
-                    [models[child_id] for child_id in child_ids]
-        return models[res_id]
+        self.conn.executemany('INSERT INTO interval VALUES (NULL, ?, ?, ? + 1)', ivl_table)
+        self.conn.executemany('INSERT INTO sublist VALUES (NULL, ? + 1, ?)', grp_table)
+        self.conn.executemany('INSERT INTO model VALUES (NULL, ? + 1, ?, ?, ?, ?, ?, ? + 1, ?, ?)', flat_mdls)
     
-    def getModelsInInterval(self, ivl):
-        cur = self.conn.cursor()
+    def _flattenModels(self, mdls):
+        def insertTranscripts(gene, gene_id):
+            for transcript_ordering, transcript in enumerate(gene.transcripts.itervalues()):
+                ivl_idx = insertInterval(transcript.ivl)
+                flat_mdls.append([ivl_idx, transcript.ivl.chr, transcript.ivl.strand, 'transcript', transcript.name, transcript_ordering, gene_id, 0, 0])
+                transcript_idx = len(flat_mdls) - 1
+                updateLeft(flat_mdls[transcript_idx])
+                insertExons(transcript, transcript_idx)
+                updateRight(flat_mdls[transcript_idx])
         
-        res_ids = []
-        models = {}
-        children = defaultdict(list)
-        qry = '''SELECT identifier.value, model.id, model.type, model.strand,
-            model.parent_id, interval.ivl_fr, interval.ivl_to
-            FROM model, interval
-            LEFT JOIN identifier
-            ON
-                identifier.type = 'PRIMARY' AND
-                identifier.model_id = model.id
-            WHERE
-                model.chr = ? AND
-                model.interval_id = interval.id AND
-                interval.ivl_fr >= ? AND
-                interval.ivl_to <= ?;'''
-        rows = cur.execute(qry, (ivl.chr, ivl.start, ivl.stop))
-        for name, id, typ, strand, parent, fr, to in rows:
-            models[id] = Model(name, interval(chm, fr, to, strand), typ)
-            if parent is None:
-                res_ids.append(id)
-            else:
-                children[parent].append(id)
-        for parent_id, child_ids in children.iteritems():
-            if parent_id in models:
-                models[parent_id].children =\
-                    [models[child_id] for child_id in child_ids]
-        return [models[res_id] for res_id in res_ids]
-
-    def getModelsOverlappingInterval(self, ivl):
-        cur = self.conn.cursor()
+        def insertExons(transcript, transcript_id):
+            for exon_ordering, exon in enumerate(transcript.exons):
+                ivl_idx = insertInterval(exon.ivl)
+                flat_mdls.append([ivl_idx, exon.ivl.chr, exon.ivl.strand, exon.type, None, exon_ordering, transcript_id, 0, 0])
+                exon_id = len(flat_mdls) - 1
+                updateLeft(flat_mdls[exon_id])
+                updateRight(flat_mdls[exon_id])
         
-        res_ids = []
-        models = {}
-        children = defaultdict(list)
-        qry = '''SELECT identifier.value, model.id, model.type, model.strand,
-            model.parent_id, interval.ivl_fr, interval.ivl_to
-            FROM model, interval
-            LEFT JOIN identifier
-            ON
-                identifier.type = 'PRIMARY' AND
-                identifier.model_id = model.id
-            WHERE
-                model.chr = ? AND
-                model.interval_id = interval.id AND
-                interval.ivl_fr <= ? AND
-                ? <= interval.ivl_to;'''
-        rows = cur.execute(qry, (ivl.chr, ivl.stop, ivl.start))
-        for name, id, typ, strand, parent, fr, to in rows:
-            models[id] = Model(name,
-                interval(ivl.chr, ivl.start, ivl.stop, strand), typ)
-            if parent is None:
-                res_ids.append(id)
-            else:
-                children[parent].append(id)
-        for parent_id, child_ids in children.iteritems():
-            if parent_id in models:
-                models[parent_id].children =\
-                    [models[child_id] for child_id in child_ids]
-        return [models[res_id] for res_id in res_ids]
-
-    def getModelsAtPosition(self, chm, pos):
+        def insertInterval(interval):
+            ivls.append(interval)
+            return len(ivls) - 1
+        
+        def updateLeft(flat_mdl):
+            flat_mdl[7] = idx_generator.next()
+        
+        def updateRight(flat_mdl):
+            flat_mdl[8] = idx_generator.next()
+        
+        flat_mdls = []
+        ivls = []
+        idx_generator = itertools.count()
+        
+        for gene_ordering, gene in enumerate(mdls):
+            ivl_idx = insertInterval(gene.ivl)
+            flat_mdls.append([ivl_idx, gene.ivl.chr, gene.ivl.strand, 'gene', gene.name, gene_ordering, None, 0, 0])
+            gene_idx = len(flat_mdls) - 1
+            updateLeft(flat_mdls[gene_idx])
+            insertTranscripts(gene, gene_idx)
+            updateRight(flat_mdls[gene_idx])
+        return flat_mdls, ivls
+    
+    def _createIndices(self):
         cur = self.conn.cursor()
-
-        res_ids = []
-        models = {}
-        children = defaultdict(list)
-        qry = '''SELECT identifier.value, model.id, model.type, model.strand,
-            model.parent_id, interval.ivl_fr, interval.ivl_to
-            FROM model, interval
-            LEFT JOIN identifier
-            ON
-                identifier.type = 'PRIMARY' AND
-                identifier.model_id = model.id
-            WHERE
-                model.chr = ? AND
-                model.interval_id = interval.id AND
-                interval.ivl_fr <= ? AND
-                interval.ivl_to > ?;'''
-        rows = cur.execute(qry, (chm, pos, pos))
-        for name, id, typ, strand, parent, fr, to in rows:
-            models[id] = Model(name, interval(chm, fr, to, strand), typ)
-            if parent is None:
-                res_ids.append(id)
-            else:
-                children[parent].append(id)
-        for parent_id, child_ids in children.iteritems():
-            if parent_id in models:
-                models[parent_id].children =\
-                    [models[child_id] for child_id in child_ids]
-        return [models[res_id] for res_id in res_ids]
-
+        cur.execute('CREATE INDEX IF NOT EXISTS model_name_idx ON model(name)')
+        cur.execute('CREATE INDEX IF NOT EXISTS model_position_idx ON model(interval, chromosome, type)')
+        cur.execute('CREATE INDEX IF NOT EXISTS model_extent_idx ON model(left, right)')
+        cur.execute('CREATE INDEX IF NOT EXISTS interval_idx ON interval(id, start, stop)')
--- lhc/test/binf/collection/model_set.py
+++ lhc/test/binf/collection/model_set.py
@@ -1,87 +1,156 @@
-'''
-Created on 05/08/2013
-
-@author: Liam Childs
-'''
-import os
-import shutil
 import unittest
 import tempfile
 
-from uuid import uuid4 as uuid
-from lhc.binf.genomic_interval import interval
-from lhc.binf.collection.model_set import ModelSet
+from collections import OrderedDict
+from lhc.binf.genomic_coordinate import Interval
+from lhc.binf.gene_model import Gene, Transcript, Exon
+from lhc.binf.collection.model_set2 import ModelSet
 
-class Test(unittest.TestCase):
+class TestModelSet(unittest.TestCase):
     
-    @classmethod
-    def setUpClass(cls):
-        cls.tmpdir = tempfile.mkdtemp()
+    def setUp(self):
+        self.hndl, self.fname = tempfile.mkstemp()
+        self.genes = [
+            Gene('00', Interval('1', 1000, 2000), OrderedDict([
+                ('00.0', Transcript('00.0', Interval('1', 1000, 2000), [
+                    Exon(Interval('1', 1000, 1200), 'CDS'),
+                    Exon(Interval('1', 1400, 1600), 'CDS'),
+                    Exon(Interval('1', 1800, 2000), 'CDS')
+                ])),
+                ('00.1', Transcript('00.1', Interval('1', 1000, 2000), [
+                    Exon(Interval('1', 1000, 1200), 'CDS'),
+                    Exon(Interval('1', 1800, 2000), 'CDS')
+                ]))
+            ])),
+            Gene('01', Interval('1', 3000, 4000), OrderedDict([
+                ('01.0', Transcript('01.0', Interval('1', 3000, 4000), [
+                    Exon(Interval('1', 3000, 3200), 'CDS'),
+                    Exon(Interval('1', 3400, 3600), 'CDS'),
+                    Exon(Interval('1', 3800, 4000), 'CDS')
+                ])),
+                ('01.1', Transcript('01.1', Interval('1', 3000, 4000), [
+                    Exon(Interval('1', 3000, 3200), 'CDS'),
+                    Exon(Interval('1', 3800, 4000), 'CDS')
+                ]))
+            ])),
+            Gene('02', Interval('1', 5000, 6000), OrderedDict([
+                ('02.0', Transcript('02.0', Interval('1', 5000, 6000), [
+                    Exon(Interval('1', 5000, 5200), 'CDS'),
+                    Exon(Interval('1', 5400, 5600), 'CDS'),
+                    Exon(Interval('1', 5800, 6000), 'CDS')
+                ])),
+                ('02.1', Transcript('02.1', Interval('1', 5000, 6000), [
+                    Exon(Interval('1', 5000, 5200), 'CDS'),
+                    Exon(Interval('1', 5800, 6000), 'CDS')
+                ]))
+            ])),
+            Gene('03', Interval('2', 5000, 6000), OrderedDict([
+                ('03.0', Transcript('03.0', Interval('2', 5000, 6000), [
+                    Exon(Interval('2', 5000, 5200), 'CDS'),
+                    Exon(Interval('2', 5400, 5600), 'CDS'),
+                    Exon(Interval('2', 5800, 6000), 'CDS')
+                ])),
+                ('03.1', Transcript('03.1', Interval('2', 5000, 6000), [
+                    Exon(Interval('2', 5000, 5200), 'CDS'),
+                    Exon(Interval('2', 5800, 6000), 'CDS')
+                ]))
+            ]))]
+        
+        self.model_set = ModelSet(self.fname, self.genes)
     
-    def getName(self):
-        return os.path.join(self.tmpdir, str(uuid())[:8])
+    def test_init(self):
+        cur = self.model_set.conn.cursor()
+        
+        qry = 'SELECT * FROM model'
+        self.assertEquals(list(cur.execute(qry)), [(1, 1, u'1', u'+', u'gene', u'00', 0, None, 0, 15),
+            (2, 1, u'1', u'+', u'transcript', u'00.0', 0, 1, 1, 8),
+            (3, 4, u'1', u'+', u'CDS', None, 0, 2, 2, 3),
+            (4, 5, u'1', u'+', u'CDS', None, 1, 2, 4, 5),
+            (5, 6, u'1', u'+', u'CDS', None, 2, 2, 6, 7),
+            (6, 1, u'1', u'+', u'transcript', u'00.1', 1, 1, 9, 14),
+            (7, 4, u'1', u'+', u'CDS', None, 0, 6, 10, 11),
+            (8, 6, u'1', u'+', u'CDS', None, 1, 6, 12, 13),
+            (9, 2, u'1', u'+', u'gene', u'01', 1, None, 16, 31),
+            (10, 2, u'1', u'+', u'transcript', u'01.0', 0, 9, 17, 24),
+            (11, 7, u'1', u'+', u'CDS', None, 0, 10, 18, 19),
+            (12, 8, u'1', u'+', u'CDS', None, 1, 10, 20, 21),
+            (13, 9, u'1', u'+', u'CDS', None, 2, 10, 22, 23),
+            (14, 2, u'1', u'+', u'transcript', u'01.1', 1, 9, 25, 30),
+            (15, 7, u'1', u'+', u'CDS', None, 0, 14, 26, 27),
+            (16, 9, u'1', u'+', u'CDS', None, 1, 14, 28, 29),
+            (17, 3, u'1', u'+', u'gene', u'02', 2, None, 32, 47),
+            (18, 3, u'1', u'+', u'transcript', u'02.0', 0, 17, 33, 40),
+            (19, 10, u'1', u'+', u'CDS', None, 0, 18, 34, 35),
+            (20, 11, u'1', u'+', u'CDS', None, 1, 18, 36, 37),
+            (21, 12, u'1', u'+', u'CDS', None, 2, 18, 38, 39),
+            (22, 3, u'1', u'+', u'transcript', u'02.1', 1, 17, 41, 46),
+            (23, 10, u'1', u'+', u'CDS', None, 0, 22, 42, 43),
+            (24, 12, u'1', u'+', u'CDS', None, 1, 22, 44, 45),
+            (25, 3, u'2', u'+', u'gene', u'03', 3, None, 48, 63),
+            (26, 3, u'2', u'+', u'transcript', u'03.0', 0, 25, 49, 56),
+            (27, 10, u'2', u'+', u'CDS', None, 0, 26, 50, 51),
+            (28, 11, u'2', u'+', u'CDS', None, 1, 26, 52, 53),
+            (29, 12, u'2', u'+', u'CDS', None, 2, 26, 54, 55),
+            (30, 3, u'2', u'+', u'transcript', u'03.1', 1, 25, 57, 62),
+            (31, 10, u'2', u'+', u'CDS', None, 0, 30, 58, 59),
+            (32, 12, u'2', u'+', u'CDS', None, 1, 30, 60, 61)
+        ])
+        
+        qry = 'SELECT * FROM interval'
+        self.assertEquals(list(cur.execute(qry)), [(1, 1000, 2000, 2),
+            (2, 3000, 4000, 3),
+            (3, 5000, 6000, 4),
+            (4, 1000, 1200, 0),
+            (5, 1400, 1600, 0),
+            (6, 1800, 2000, 0),
+            (7, 3000, 3200, 0),
+            (8, 3400, 3600, 0),
+            (9, 3800, 4000, 0),
+            (10, 5000, 5200, 0),
+            (11, 5400, 5600, 0),
+            (12, 5800, 6000, 0)
+        ])
+        
+        qry = 'SELECT * FROM sublist'
+        self.assertEquals(list(cur.execute(qry)), [(1, 1, 3),
+            (2, 4, 3),
+            (3, 7, 3),
+            (4, 10, 3)
+        ])
 
-    def testAddSegment(self):
-        mset = ModelSet(self.getName(), 'w')
-        ivl = interval('chr1', 0, 10, '+')
-        model_id = mset.addModelSegment(ivl, 'gene')
+    def test_get(self):
+        gene = self.model_set.get('00')
+        self.assertEquals(gene.name, '00')
+        self.assertEquals(gene.ivl, Interval('1', 1000, 2000))
+        self.assertEquals(gene.transcripts.keys(), ['00.0', '00.1'])
+        self.assertEquals(gene.transcripts['00.0'].ivl, Interval('1', 1000, 2000))
+        self.assertEquals(gene.transcripts['00.1'].ivl, Interval('1', 1000, 2000))
+        self.assertEquals(len(gene.transcripts['00.0'].exons), 3)
+        self.assertEquals([exon.ivl for exon in gene.transcripts['00.0'].exons],
+            [Interval('1', 1000, 1200), Interval('1', 1400, 1600), Interval('1', 1800, 2000)])
+        self.assertEquals(len(gene.transcripts['00.1'].exons), 2)
+        self.assertEquals([exon.ivl for exon in gene.transcripts['00.1'].exons],
+            [Interval('1', 1000, 1200), Interval('1', 1800, 2000)])
         
-        qry = 'SELECT * FROM model WHERE id = ?'
-        row = mset.conn.execute(qry, (model_id,)).fetchone()
-        self.assertEquals(row, (model_id, ivl.chr, 1, 'gene', ivl.strand, None))
+        gene = self.model_set.get('01')
+        self.assertEquals(gene.name, '01')
+        self.assertEquals(gene.ivl, Interval('1', 3000, 4000))
+        self.assertEquals(gene.transcripts.keys(), ['01.0', '01.1'])
+        self.assertEquals(gene.transcripts['01.0'].ivl, Interval('1', 3000, 4000))
+        self.assertEquals(gene.transcripts['01.1'].ivl, Interval('1', 3000, 4000))
+        self.assertEquals(len(gene.transcripts['01.0'].exons), 3)
+        self.assertEquals([exon.ivl for exon in gene.transcripts['01.0'].exons],
+            [Interval('1', 3000, 3200), Interval('1', 3400, 3600), Interval('1', 3800, 4000)])
+        self.assertEquals(len(gene.transcripts['01.1'].exons), 2)
+        self.assertEquals([exon.ivl for exon in gene.transcripts['01.1'].exons],
+            [Interval('1', 3000, 3200), Interval('1', 3800, 4000)])
+     
+    def test_intersect(self):
+        res = self.model_set.intersect(Interval('1', 1000, 6000))
+        self.assertEquals(len(res), 3)
         
-        qry = 'SELECT * FROM interval WHERE id = ?'
-        interval_id = row[2]
-        row = mset.conn.execute(qry, (interval_id,)).fetchone()
-        self.assertEquals(row, (interval_id, ivl.start, ivl.stop))
-    
-    def testAddIdentifier(self):
-        mset = ModelSet(self.getName(), 'w')
-        ivl = interval('chr1', 0, 10, '+')
-        model_id = mset.addModelSegment(ivl, 'gene')
-        identifier_id = mset.addIdentifier(model_id, 'GENE_A')
-        
-        qry = 'SELECT * FROM identifier WHERE id = ?'
-        row = mset.conn.execute(qry, (identifier_id,)).fetchone()
-        self.assertEquals(row, (identifier_id, model_id, 'GENE_A', 'PRIMARY'))
-    
-    def testGetByIdentifier(self):
-        mset = ModelSet(self.getName(), 'w')
-        ivl = interval('chr1', 0, 10, '+')
-        model_id = mset.addModelSegment(ivl, 'gene')
-        mset.addIdentifier(model_id, 'GENE_A')
-        
-        res = mset['GENE_A']
-        
-        self.assertEquals((res.name, res.ivl, res.type), ('GENE_A', ivl, 'gene'))
-    
-    def testGetComplexModel(self):
-        mset = ModelSet(self.getName(), 'w')
-        ivl = interval('chr1', 0, 100, '+')
-        parent_id = mset.addModelSegment(ivl, 'gene')
-        mset.addIdentifier(parent_id, 'GENE_A')
-        ivl = interval('chr1', 0, 100, '+')
-        model_id = mset.addModelSegment(ivl, 'transcript', parent_id)
-        mset.addIdentifier(model_id, 'GENE_A.0')
-        ivl = interval('chr1', 0, 50, 100, '+')
-        mset.addModelSegment(ivl, 'exon', model_id)
-        ivl = interval('chr1', 60, 100, '+')
-        mset.addModelSegment(ivl, 'exon', model_id)
-        model_id = mset.addModelSegment(ivl, 'transcript', parent_id)
-        mset.addIdentifier(model_id, 'GENE_A.1')
-        ivl = interval('chr1', 0, 40, '+')
-        mset.addModelSegment(ivl, 'exon', model_id)
-        ivl = interval('chr1', 50, 100, '+')
-        mset.addModelSegment(ivl, 'exon', model_id)
-
-        res = mset['GENE_A']
-        
-        self.assertEquals(len(res.children), 2)
-        self.assertEquals(set(child.name for child in res.children), set(('GENE_A.0', 'GENE_A.1')))
-    
-    @classmethod
-    def tearDownClass(cls):
-        shutil.rmtree(cls.tmpdir)
+        res = self.model_set.intersect(Interval('2', 1000, 6000))
+        self.assertEquals(len(res), 1)
 
 if __name__ == "__main__":
     unittest.main()
