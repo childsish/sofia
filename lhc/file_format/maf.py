65e43a90f6b21655d0d301e82194adc63c432c94
Liam Childs <liam_childs@hotmail.com>
2014-02-28 23:26:16 +0100

    maf and vcf parser got too complicated
    reduced to bare essentials that use lhc classes

--- lhc/file_format/maf.py
+++ lhc/file_format/maf.py
@@ -1,99 +1,40 @@
-import os
 import argparse
+import os
 
-from csv import iterCsv, ColumnBuilder, FieldBuilder
 from lhc.filepool import FilePool
-from lhc.tools import enum
-from lhc.binf.genomic_coordinate import Interval, Position
+from lhc.binf.variant import Variant
+from lhc.binf.genomic_coordinate import Interval
+
+CHR = 4
+START = 5
+STOP = 6
+TYPE = 9
+REF = 10
+ALT1 = 11
+ALT2 = 12
+GENOTYPE = 15
+
+def iterMaf(fname):
+    infile = open(fname)
+    infile.readline()
+    for line in infile:
+        parts = line.split('\t')
+        yield Variant(Interval(parts[CHR], int(parts[START]) - 1, int(parts[STOP])),
+            [parts[ALT1], parts[ALT2]], [parts[TYPE], parts[TYPE]],
+            0.0, parts[GENOTYPE])
+    infile.close()
 
 def main(argv):
-    def splitMafWrapper(args):
-        splitMaf(args.input_file, args.output_directory)
-    
     parser = argparse.ArgumentParser(description='Mutation Annotation File functions')
     subparsers = parser.add_subparsers(help='sub-command help')
     
     split_parser = subparsers.add_parser('split', help='Split MAF help')
     split_parser.add_argument('input_file', help='The MAF file to split')
     split_parser.add_argument('-o', '--output_directory', help='The directory to place the output')
-    split_parser.set_defaults(func=splitMafWrapper)
+    split_parser.set_defaults(func=lambda args:splitMaf(args.input_file, args.output_directory))
     
     args = parser.parse_args(argv[1:])
     args.func(args)
-
-VARIANT_TYPE = enum(['VARIANT', 'SNP'])
-
-class MafParser(object):
-
-    CHR = 4
-    POS = 5
-    FR = 5
-    TO = 6
-    STRAND = 7
-    TYPE = 9
-    REF = 10
-    ALT1 = 11
-    ALT2 = 12
-    GENOTYPE = 15
-
-    def __init__(self, type=VARIANT_TYPE.VARIANT):
-        self.type = type
-
-        self.column_builder = ColumnBuilder()
-        self.column_builder.registerColumn('chr', MafParser.CHR)
-        self.column_builder.registerColumn('fr', MafParser.FR, int)
-        self.column_builder.registerColumn('to', MafParser.TO, int)
-        self.column_builder.registerColumn('strand', MafParser.STRAND)
-        self.column_builder.registerColumn('type', MafParser.TYPE)
-        self.column_builder.registerColumn('ref', MafParser.REF)
-        self.column_builder.registerColumn('alt1', MafParser.ALT1)
-        self.column_builder.registerColumn('alt2', MafParser.ALT2)
-        self.column_builder.registerColumn('genotype_id', MafParser.GENOTYPE)
-
-        #TODO: Find a better place
-        self.field_builder = FieldBuilder()
-        self.field_builder.registerField('ivl', self._parseInterval)
-        self.field_builder.registerField('type')
-        self.field_builder.registerField('ref')
-        self.field_builder.registerField('alt1')
-        self.field_builder.registerField('alt2')
-        self.field_builder.registerField('genotype_id')
-        
-        if type == VARIANT_TYPE.SNP:
-            self.snp_field_builder = FieldBuilder()
-            self.snp_field_builder.registerField('pos', self._parsePosition)
-            self.snp_field_builder.registerField('ref')
-            self.snp_field_builder.registerField('alt1')
-            self.snp_field_builder.registerField('alt2')
-            self.snp_field_builder.registerField('genotype_id')
-    
-    def iterFile(self, fname, skip=1):
-        it = iterCsv(fname, self.column_builder, self.field_builder, skip)
-        if self.type == VARIANT_TYPE.VARIANT:
-            for row in it:
-                yield row
-        
-        for row in it:
-            if row.type == 'SNP':
-                pos = Position(row.ivl.chr, row.ivl.start, row.ivl.strand)
-                yield self.snp_field_builder.tuple._make([pos, row.ref,
-                    row.alt1, row.alt2, row.genotype_id])
-            elif row.type == 'DNP':
-                for i in xrange(len(row.ref)):
-                    pos = Position(row.ivl.chr, row.ivl.start + i,
-                        row.ivl.strand)
-                    yield self.snp_field_builder.tuple._make([pos, row.ref[i],
-                        row.alt1[i], row.alt2[i], row.genotype_id])
-            elif row.type in ('DEL', 'INS'):
-                continue
-            else:
-                raise ValueError('Unrecognised variant type: %s'%row.type)
-    
-    def _parseInterval(self, cols):
-        return Interval(cols.chr, cols.fr - 1, cols.to, cols.strand)
-    
-    def _parsePosition(self, cols):
-        return Position(cols.chr, cols.fr - 1, cols.strand)
     
 def splitMaf(fname, outdir=None):
     if outdir is None:
@@ -108,7 +49,7 @@ def splitMaf(fname, outdir=None):
         if line[0] == '#':
             continue
         
-        genotype_id = line.split('\t')[15]
+        genotype_id = line.split('\t')[GENOTYPE]
         key = os.path.join(outdir, genotype_id + '.maf')
         if key not in file_pool:
             file_pool[key].write(hdr)
--- lhc/file_format/vcf.py
+++ lhc/file_format/vcf.py
@@ -1,75 +1,38 @@
 import os
 
-from csv import iterCsv, ColumnBuilder, FieldBuilder
-from lhc.tools import enum
+from functools import partial
+from lhc.binf.variant import Variant
 from lhc.binf.genomic_coordinate import Interval
 
-VARIANT_TYPE = enum(['VARIANT', 'SNP'])
+CHR = 0
+START = 1
+STOP = 1
+ID = 2
+REF = 3
+ALT1 = 4
+ALT2 = 4
+QUAL = 5
+ATTR = 7
 
-class VcfParser(object):
-
-    CHR = 0
-    POS = 1
-    ID = 2
-    REF = 3
-    ALT1 = 4
-    ALT2 = 4
-    QUAL = 5
-    ATTR = 7
-
-    def __init__(self, type=VARIANT_TYPE.SNP):
-        self.type = type
-
-        self.column_builder = ColumnBuilder()
-        self.column_builder.registerColumn('chr', VcfParser.CHR)
-        self.column_builder.registerColumn('pos', VcfParser.POS, int)
-        self.column_builder.registerColumn('id', VcfParser.ID)
-        self.column_builder.registerColumn('ref', VcfParser.REF)
-        self.column_builder.registerColumn('alt', VcfParser.ALT1)
-        self.column_builder.registerColumn('qual', VcfParser.QUAL)
-        self.column_builder.registerColumn('attr', VcfParser.ATTR)
-        
-        #TODO: Find a better place
-        self.field_builder = FieldBuilder()
-        if type == VARIANT_TYPE.SNP:
-            self.field_builder.registerField('id')
-            self.field_builder.registerField('pos', self._parsePosition)
-            self.field_builder.registerField('ref')
-            self.field_builder.registerField('alt1', lambda cols:cols.alt)
-            self.field_builder.registerField('alt2', lambda cols:cols.alt)
-            self.field_builder.registerField('qual', self._parseQuality)
-            self.field_builder.registerField('attr', self._parseAttributes)
-            self.field_builder.registerField('genotype_id',
-                lambda cols:os.basename(fname).rsplit('.')[0])
-        elif type == VARIANT_TYPE.VARIANT:
-            self.field_builder.registerField('id')
-            self.field_builder.registerField('ivl', self._parseInterval)
-            self.field_builder.registerField('type', lambda cols:'SNP')
-            self.field_builder.registerField('ref')
-            self.field_builder.registerField('alt1', lambda cols:cols.alt)
-            self.field_builder.registerField('alt2', lambda cols:cols.alt)
-            self.field_builder.registerField('qual', self._parseQuality)
-            self.field_builder.registerField('attr', self._parseAttributes)
-            self.field_builder.registerField('genotype_id',
-                lambda cols:os.basename(fname).rsplit('.')[0])
-        else:
-            raise ValueError('Unrecognised variant type: %s'%type)
-        
-    def iterVariants(self, fname, skip=0):
-        return iterCsv(fname, self.column_builder, self.field_builder, skip)
+def iterVcf(fname):
+    def parseType(ref, alt):
+        if len(ref) == 1 and len(alt) == 1:
+            return 'SNP'
+        elif len(ref) == len(alt):
+            return 'MNP'
+        elif len(ref) > len(alt):
+            return 'DEL'
+        elif len(ref) < len(alt):
+            return 'INS'
+        raise ValueError('Unknown mutation type: %s %s'%(ref, alt))
     
-    def _parsePosition(self, cols):
-        return Position(cols.chr, cols.pos - 1, cols)
-
-    def _parseInterval(self, cols):
-        return Interval(cols.chr, cols.pos - 1, cols.pos)
-
-    def _parseQuality(self, cols):
-        if cols.qual == '.':
-            return None
-        return float(cols.qual)
-
-    def _parseAttributes(self, cols):
-        return dict(part.strip().split('=', 1)\
-            for part in cols.attr.strip().split(';'))
-
+    genotype = os.path.basename(fname.rsplit('.', 1)[0])
+    infile = open(fname)
+    for line in infile:
+        parts = line.split('\t')
+        alts = parts[ALT1].split(',')
+        ref = parts[REF]
+        types = map(partial(parseType, ref=ref), alts)
+        yield Variant(Interval(parts[CHR], int(parts[START]) - 1, int(parts[START]) + max(map(len, alts)) - 1),
+            alts, types, float(parts[QUAL]), genotype)
+    infile.close()
