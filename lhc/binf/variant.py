2060cbde7308f710da05caa027b74abe93f263ad
Liam Childs <liam_childs@hotmail.com>
2014-02-28 23:27:24 +0100

    added ordering to variant

--- lhc/binf/variant.py
+++ lhc/binf/variant.py
@@ -1,13 +1,32 @@
+from functools import total_ordering
 from lhc.tools import enum
 
+@total_ordering
 class Variant:
     
     TYPES = enum(['SNP', 'MNP', 'DEL', 'INS'])
     
-    def __init__(self, genotype, ivl, alt, quality, allele=1, type=Variant.TYPES.SNP):
-        self.genotype = genotype
+    def __init__(self, ivl, alt, type=[TYPES.SNP], quality=0, genotype=None):
+        """Create a variant for a given genotype at a given position
+        
+        :param interval ivl: the position/interval of the variation
+        :param list alt: a list of the alleles found at this position. Usually one is different from the reference genomes
+        """
         self.ivl = ivl
-        self.alt = alt
-        self.quality = quality
-        self.allele = allele
+        self.alt = map(str, alt)
         self.type = type
+        self.quality = quality
+        self.genotype = genotype
+    
+    def __str__(self):
+        return '%s %s %s %s'%(self.ivl, self.alt, self.type, self.genotype)
+
+    def __eq__(self, other):
+        return self.ivl == other.ivl and\
+            self.alt == other.alt and\
+            self.type == other.type and\
+            self.genotype == other.genotype
+
+    def __lt__(self, other):
+        return self.chr < other.chr or\
+            self.chr == other.chr and self.start < other.start
