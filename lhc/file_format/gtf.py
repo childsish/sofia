2e3d2841bbaf563c98b8ef65884e44aadb3753f3
Liam Childs <liam_childs@hotmail.com>
2014-03-24 16:26:00 +0100

    updated gtf parser

--- lhc/file_format/gtf.py
+++ lhc/file_format/gtf.py
@@ -1,31 +1,53 @@
-from csv import iterCsv, ColumnBuilder, FieldBuilder
+from collections import namedtuple
 from lhc.binf.genomic_coordinate import Interval
+from lhc.binf.gene_model import Gene, Transcript, Exon
 
-def iterGtf(fname):
-    column_builder = ColumnBuilder()
-    column_builder.registerColumn('chr', 0)
-    column_builder.registerColumn('fr', 3, int)
-    column_builder.registerColumn('to', 4, int)
-    column_builder.registerColumn('strand', 6)
-    column_builder.registerColumn('type', 2)
-    column_builder.registerColumn('attr', 8)
-    
-    field_builder = FieldBuilder()
-    field_builder.registerField('ivl', parseInterval)
-    field_builder.registerField('type')
-    field_builder.registerField('attr', parseAttributes)
-    
-    for fields in iterCsv(fname, column_builder, field_builder):
-        yield fields
+CHR = 0
+SRC = 1
+TYPE = 2
+START = 3
+STOP = 4
+SCORE = 5
+STRAND = 6
+PHASE = 7
+ATTR = 8
 
-def parseInterval(cols):
-    return Interval(cols.chr, cols.fr - 1, cols.to, cols.strand)
+def iterModels(fname):
+    gene = None
+    for entry in iterEntries(fname):
+        ivl = Interval(entry.chr, entry.start, entry.stop)
+        if entry.type == 'gene':
+            if gene is not None:
+                yield gene
+            gene = Gene(entry.attr['gene_name'], ivl)
+        elif entry.type == 'transcript':
+            gene.transcripts[entry.attr['transcript_name']] =\
+                Transcript(entry.attr['transcript_name'], ivl)
+        elif entry.type == 'exon':
+            gene.transcripts.values()[-1].exons.append(Exon(ivl, 'CDS'))
+        
+GtfEntry = namedtuple('GtfEntry', ('chr', 'src', 'type', 'start', 'stop',
+    'score', 'strand', 'phase', 'attr'))
+def iterEntries(fname):
+    infile = open(fname)
+    for line in infile:
+        if line.startswith('#'):
+            continue
+        parts = line.split('\t')
+        parts[START] = int(parts[START]) - 1
+        parts[STOP] = int(parts[STOP])
+        parts[ATTR] = parseAttributes(parts[ATTR])
+        yield GtfEntry(*parts)
+    infile.close()
 
-def parseAttributes(cols):
-    parts = [part.strip().split(' ', 1) for part in cols.attr.split(';') if part != '']
+def parseAttributes(attr):
+    parts = [part.strip().split(' ', 1)
+        for part in attr.split(';')
+        if part.strip() != '']
     for i in xrange(len(parts)):
         if parts[i][1].startswith('"'):
             parts[i][1] = parts[i][1][1:-1]
         else:
             parts[i][1] = int(parts[i][1])
     return dict(parts)
+
