caa6ae587c42d10ef9acc929a1443db7df40ec18
Liam Childs <liam_childs@hotmail.com>
2014-04-09 10:50:26 +0200

    standardised gtf

--- lhc/file_format/gtf.py
+++ lhc/file_format/gtf.py
@@ -1,53 +1,167 @@
+import cPickle
+import os
+
 from collections import namedtuple
 from lhc.binf.genomic_coordinate import Interval
 from lhc.binf.gene_model import Gene, Transcript, Exon
+from lhc.file_format.entry_set import EntrySet
+from lhc.indices.index import Index
+from lhc.indices.exact_string import ExactStringIndex
+from lhc.indices.overlapping_interval import OverlappingIntervalIndex
 
-CHR = 0
-SRC = 1
-TYPE = 2
-START = 3
-STOP = 4
-SCORE = 5
-STRAND = 6
-PHASE = 7
-ATTR = 8
-
-def iterModels(fname):
-    gene = None
-    for entry in iterEntries(fname):
-        ivl = Interval(entry.chr, entry.start, entry.stop, entry.strand)
-        if entry.type == 'gene':
-            if gene is not None:
-                yield gene
-            gene = Gene(entry.attr['gene_name'], ivl)
-        elif entry.type == 'transcript':
-            gene.transcripts[entry.attr['transcript_name']] =\
-                Transcript(entry.attr['transcript_name'], ivl)
-        elif entry.type == 'CDS':
-            gene.transcripts.values()[-1].exons.append(Exon(ivl, 'CDS'))
-        
 GtfEntry = namedtuple('GtfEntry', ('chr', 'src', 'type', 'start', 'stop',
     'score', 'strand', 'phase', 'attr'))
-def iterEntries(fname):
-    infile = open(fname)
-    for line in infile:
-        if line.startswith('#'):
-            continue
-        parts = line.split('\t')
-        parts[START] = int(parts[START]) - 1
-        parts[STOP] = int(parts[STOP])
-        parts[ATTR] = parseAttributes(parts[ATTR])
-        yield GtfEntry(*parts)
-    infile.close()
 
-def parseAttributes(attr):
-    parts = [part.strip().split(' ', 1)
-        for part in attr.split(';')
-        if part.strip() != '']
-    for i in xrange(len(parts)):
-        if parts[i][1].startswith('"'):
-            parts[i][1] = parts[i][1][1:-1]
+class GtfParser(EntrySet):
+    
+    CHR = 0
+    SRC = 1
+    TYPE = 2
+    START = 3
+    STOP = 4
+    SCORE = 5
+    STRAND = 6
+    PHASE = 7
+    ATTR = 8
+    
+    def __init__(self, fname, iname=None):
+        self.fname = fname
+        self.iname = self.getIndexName(fname) if iname is None else iname
+        self.key_index = None
+        self.ivl_index = None
+        self.data = None
+    
+    def __getitem__(self, key):
+        if os.path.exists(self.iname):
+            if self.key_index is None:
+                infile = open(self.iname)
+                self.key_index = cPickle.load(infile)
+                self.ivl_index = cPickle.load(infile)
+                infile.close()
+            return self._getIndexedData(key)
+        elif self.data is None:
+            self.key_index = ExactStringIndex()
+            self.ivl_index = Index((ExactStringIndex, OverlappingIntervalIndex))
+            self.data = list(iter(self))
+            for i, entry in enumerate(self.data):
+                self.key_index[entry.name] = i
+                self.ivl_index[(entry.ivl.chr, entry.ivl)] = i
+        
+        if isinstance(key, basestring):
+            return self.data[self.key_index[key]]
+        elif hasattr(key, 'chr') and hasattr(key, 'start') and hasattr(key, 'stop'):
+            return [self.data[v] for k, v in self.ivl_index[(key.chr, key)]]
+        raise NotImplementedError('Random access not implemented for %s'%type(key))
+
+    def _iterHandle(self, infile):
+        gene = None
+        for line in self._iterLines(infile):
+            ivl = Interval(line.chr, line.start, line.stop, line.strand)
+            if line.type == 'gene':
+                if gene is not None:
+                    yield gene
+                gene = Gene(line.attr['gene_name'], ivl)
+            elif line.type == 'transcript':
+                gene.transcripts[line.attr['transcript_name']] =\
+                    Transcript(line.attr['transcript_name'], ivl)
+            elif line.type == 'CDS':
+                gene.transcripts.values()[-1].exons.append(Exon(ivl, 'CDS'))
+        yield gene
+
+    def _iterLines(self, infile):
+        for line in infile:
+            if line.startswith('#') or line.strip() == '':
+                continue
+            yield self._parseLine(line)
+
+    def _getIndexedData(self, key):
+        infile = open(self.fname)
+        res = []
+        if isinstance(key, basestring):
+            fpos = self.key_index[key]
+            infile.seek(fpos)
+            res = self._iterHandle(infile).next()
+        elif hasattr(key, 'chr') and hasattr(key, 'start') and hasattr(key, 'stop'):
+            fposs = self.ivl_index[(key.chr, key)]
+            for fpos in fposs:
+                infile.seek(fpos.value)
+                res.append(self._iterHandle(infile).next())
         else:
-            parts[i][1] = int(parts[i][1])
-    return dict(parts)
+            raise NotImplementedError('Random access not implemented for %s'%type(key))
+        infile.close()
+        return res
+    
+    @classmethod
+    def _parseLine(cls, line):
+        parts = line.split('\t')
+        parts[cls.START] = int(parts[cls.START]) - 1
+        parts[cls.STOP] = int(parts[cls.STOP])
+        parts[cls.ATTR] = cls._parseAttributes(parts[cls.ATTR])
+        return GtfEntry(*parts)
+    
+    @classmethod
+    def _parseAttributes(cls, attr):
+        parts = [part.strip().split(' ', 1)
+            for part in attr.split(';')
+            if part.strip() != '']
+        for i in xrange(len(parts)):
+            if parts[i][1].startswith('"'):
+                parts[i][1] = parts[i][1][1:-1]
+            else:
+                parts[i][1] = int(parts[i][1])
+        return dict(parts)
 
+def iterEntries(fname):
+    parser = GtfParser(fname)
+    return iter(parser)
+
+def index(fname):
+    outfile = open(GtfParser.getIndexName(fname), 'wb')
+    cPickle.dump(_createKeyIndex(fname), outfile)
+    cPickle.dump(_createIvlIndex(fname), outfile)
+    outfile.close()
+
+def _createKeyIndex(fname):
+    index = ExactStringIndex()
+    infile = open(fname, 'rb')
+    while True:
+        fpos = infile.tell()
+        line = infile.readline()
+        if line == '':
+            break
+        elif line.strip() == '':
+            continue
+        entry = GtfParser._parseLine(line)
+        if entry.type == 'gene':
+            index[entry.attr['gene_name']] = fpos
+    infile.close()
+    return index
+
+def _createIvlIndex(fname):
+    index = Index((ExactStringIndex, OverlappingIntervalIndex))
+    infile = open(fname, 'rb')
+    while True:
+        fpos = infile.tell()
+        line = infile.readline()
+        if line == '':
+            break
+        elif line.strip() == '':
+            continue
+        entry = GtfParser._parseLine(line)
+        if entry.type == 'gene':
+            index[(entry.chr, entry)] = fpos
+    infile.close()
+    return index
+
+def main():
+    parser = getArgumentParser()
+    args = parser.parse_args()
+    args.func(args)
+
+def getArgumentParser():
+    parser = ArgumentParser()
+    return parser
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main())
--- lhc/test/file_format/gtf.py
+++ lhc/test/file_format/gtf.py
@@ -1,27 +1,103 @@
-'''
-Created on 06/08/2013
-
-@author: childsli
-'''
+import os
+import tempfile
 import unittest
 
-from lhc.file_format.gtf import iterGtf
 from lhc.binf.genomic_coordinate import Interval
+from lhc.file_format import gtf
 
-class Test(unittest.TestCase):
-
-    def testIterGtf(self):
-        fname = '/vol/home-vol3/wbi/childsli/data/annotation/hg19/gencode.v17.annotation.gtf'
-        row = iterGtf(fname).next()
-        self.assertEquals(row.ivl, interval('chr1', 11868, 14412, '+'))
-        self.assertEquals(row.type, 'gene')
-        self.assertEquals(row.attr, {'gene_status': 'KNOWN',
-            'havana_gene': 'OTTHUMG00000000961.2', 'level': 2,
-            'transcript_type': 'pseudogene', 'gene_id': 'ENSG00000223972.4',
-            'transcript_id': 'ENSG00000223972.4', 'transcript_name': 'DDX11L1',
-            'gene_type': 'pseudogene', 'transcript_status': 'KNOWN',
-            'gene_name': 'DDX11L1'})
-
-if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
-    unittest.main()
+class TestGtf(unittest.TestCase):
+    
+    def setUp(self):
+        fhndl, self.fname = tempfile.mkstemp()
+        os.write(fhndl, 'chr1\t.\tgene\t1000\t2000\t0\t+\t0\tgene_name "a"\n' +\
+            'chr1\t.\ttranscript\t1000\t2000\t0\t+\t0\tgene_name "a";transcript_name "a.0"\n' +\
+            'chr1\t.\tCDS\t1100\t1300\t0\t+\t0\tgene_name "a";transcript_name "a.0"\n' +\
+            'chr1\t.\tCDS\t1330\t1600\t0\t+\t0\tgene_name "a";transcript_name "a.0"\n' +\
+            'chr1\t.\tCDS\t1660\t1900\t0\t+\t0\tgene_name "a";transcript_name "a.0"\n' +\
+            'chr1\t.\ttranscript\t1000\t2000\t0\t+\t0\tgene_name "a";transcript_name "a.1"\n' +\
+            'chr1\t.\tCDS\t1100\t1300\t0\t+\t0\tgene_name "a";transcript_name "a.1"\n' +\
+            'chr1\t.\tCDS\t1660\t1900\t0\t+\t0\tgene_name "a";transcript_name "a.1"\n' +\
+            'chr1\t.\tgene\t5000\t6000\t0\t+\t0\tgene_name "b"\n' +\
+            'chr1\t.\ttranscript\t5000\t6000\t0\t+\t0\tgene_name "b";transcript_name "b.0"\n' +\
+            'chr1\t.\tCDS\t5100\t5900\t0\t+\t0\tgene_name "b";transcript_name "b.0"\n')
+        os.close(fhndl)
+    
+    def test_iterEntries(self):
+        it = gtf.iterEntries(self.fname)
+        
+        gene = it.next()
+        self.assertEquals(gene.name, 'a')
+        self.assertEquals(len(gene.transcripts), 2)
+        self.assertEquals(gene.transcripts.values()[0].name, 'a.0')
+        self.assertEquals(gene.transcripts.values()[1].name, 'a.1')
+        self.assertEquals(len(gene.transcripts.values()[0].exons), 3)
+        self.assertEquals(len(gene.transcripts.values()[1].exons), 2)
+        
+        gene = it.next()
+        self.assertEquals(gene.name, 'b')
+        self.assertEquals(len(gene.transcripts), 1)
+        self.assertEquals(gene.transcripts.values()[0].name, 'b.0')
+        self.assertEquals(len(gene.transcripts.values()[0].exons), 1)
+        
+        self.assertRaises(StopIteration, it.next)
+    
+    def test_getItemByKey(self):
+        parser = gtf.GtfParser(self.fname)
+        
+        gene = parser['a']
+        self.assertEquals(gene.name, 'a')
+        self.assertEquals(len(gene.transcripts), 2)
+        self.assertEquals(gene.transcripts.values()[0].name, 'a.0')
+        self.assertEquals(gene.transcripts.values()[1].name, 'a.1')
+        self.assertEquals(len(gene.transcripts.values()[0].exons), 3)
+        self.assertEquals(len(gene.transcripts.values()[1].exons), 2)
+        
+        gene = parser['b']
+        self.assertEquals(gene.name, 'b')
+        self.assertEquals(len(gene.transcripts), 1)
+        self.assertEquals(gene.transcripts.values()[0].name, 'b.0')
+        self.assertEquals(len(gene.transcripts.values()[0].exons), 1)
+     
+    def test_getItemInterval(self):
+        parser = gtf.GtfParser(self.fname)
+        
+        genes = parser[Interval('chr1', 500, 1500)]
+        self.assertEquals(len(genes), 1)
+        self.assertEquals(genes[0].name, 'a')
+        
+        genes = parser[Interval('chr1', 1500, 5500)]
+        self.assertEquals(len(genes), 2)
+        self.assertEquals(set(gene.name for gene in genes), set('ab'))
+ 
+    def test_getItemIndexedByKey(self):
+        gtf.index(self.fname)
+        parser = gtf.GtfParser(self.fname)
+        
+        gene = parser['a']
+        self.assertEquals(gene.name, 'a')
+        self.assertEquals(len(gene.transcripts), 2)
+        self.assertEquals(gene.transcripts.values()[0].name, 'a.0')
+        self.assertEquals(gene.transcripts.values()[1].name, 'a.1')
+        self.assertEquals(len(gene.transcripts.values()[0].exons), 3)
+        self.assertEquals(len(gene.transcripts.values()[1].exons), 2)
+        
+        gene = parser['b']
+        self.assertEquals(gene.name, 'b')
+        self.assertEquals(len(gene.transcripts), 1)
+        self.assertEquals(gene.transcripts.values()[0].name, 'b.0')
+        self.assertEquals(len(gene.transcripts.values()[0].exons), 1)
+    
+    def test_getItemIndexedInterval(self):
+        gtf.index(self.fname)
+        parser = gtf.GtfParser(self.fname)
+        
+        genes = parser[Interval('chr1', 500, 1500)]
+        self.assertEquals(len(genes), 1)
+        self.assertEquals(genes[0].name, 'a')
+        
+        genes = parser[Interval('chr1', 1500, 5500)]
+        self.assertEquals(len(genes), 2)
+        self.assertEquals(set(gene.name for gene in genes), set('ab'))
+    
+    def tearDown(self):
+        os.remove(self.fname)
