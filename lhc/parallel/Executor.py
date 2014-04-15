3c13881759973a9217b64e6906245cc9e777fc82
Liam Childs <liam_childs@hotmail.com>
2014-04-16 00:01:42 +0200

    removed senescent code

--- lhc/parallel/Executor.py
+++
@@ -1,3 +0,0 @@
-class Executor(object):
-	def __init__(self, base):
-		self.base = base
--- lhc/parallel/Job.py
+++
@@ -1,6 +0,0 @@
-class Job(object):
-	def __init__(self, cmd, stdin=None, stdout=None):
-		self.cmd = cmd
-		self.stdin = stdin
-		self.stdout = stdout
-
