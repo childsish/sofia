0aa3f06ae27bbbd73fe5567ebfadf86f4b9e56b7
Liam Childs <liam_childs@hotmail.com>
2014-04-30 17:19:43 +0200

    merged locus features into a file

--- features/locus.py
+++ features/locus.py
@@ -1,6 +1,20 @@
 from modules.feature import Feature
 from resource import DynamicResource
 
+class Chromosome(Feature):
+    
+    NAME = 'chr'
+    RESOURCES = ['locus']
+    DEPENDENCIES = [
+        {'name': 'locus',
+         'feature': DynamicResource,
+         'resource_map': {'name': 'locus'}
+        }
+    ]
+    
+    def calculate(self, locus):
+        return locus.chr
+
 class Position(Feature):
     
     NAME = 'pos'
@@ -19,3 +33,18 @@ class Position(Feature):
             return locus.start + 1
         msg = 'Can not extract position feature from type: {type}'
         raise TypeError(msg.format(type=type(locus)))
+
+class Quality(Feature):
+    
+    NAME = 'qual'
+    RESOURCES = ['locus']
+    DEPENDENCIES = [
+        {'name': 'locus',
+         'feature': DynamicResource,
+         'resource_map': {'name': 'locus'}
+        }
+    ]
+
+    def calculate(self, locus):
+        return locus.qual
+
