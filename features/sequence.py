aad48446ce091ae2aea9a0bdd63bd540f5c88569
Liam Childs <liam_childs@hotmail.com>
2014-04-30 10:09:49 +0200

    added new features

--- features/sequence.py
+++ features/sequence.py
@@ -1,5 +1,7 @@
+from itertools import izip
 from modules.feature import Feature
 from resource import StaticResource, DynamicResource
+from lhc.binf.genetic_code import GeneticCodes
 
 class Sequence(Feature):
     
@@ -28,3 +30,30 @@ class Sequence(Feature):
             return ''
         return str(entity)
 
+class ProteinSequence(Feature):
+    
+    NAME = 'protein'
+    RESOURCES = ['mdl', 'seq']
+    DEPENDENCIES = [
+        {'name': 'seq',
+         'feature': Sequence,
+         'resource_map': {'mdl': 'mdl', 'seq': 'seq'}
+        }
+    ]
+    
+    def __init__(self, resource_map, resources=None):
+        super(ProteinSequence, self).__init__(resource_map, resources)
+        self.gc = GeneticCodes()[1]
+    
+    def calculate(self, seq):
+        res = {}
+        for hdr, seq in seq.iteritems():
+            it = iter(seq)
+            protein = (self.gc[codon] for codon in izip(it, it, it))
+            res[hdr] = ''.join(protein)
+        return res
+        
+    def format(self, entity):
+        if len(entity) == 0:
+            return ''
+        return str(entity)
