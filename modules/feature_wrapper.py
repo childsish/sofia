9019a6cd79c9f12502d34713a57be4bf532edf05
Liam Childs <liam_childs@hotmail.com>
2014-06-05 22:01:42 +0200

    feature wrapper instantiates with a name

--- modules/feature_wrapper.py
+++ modules/feature_wrapper.py
@@ -1,25 +1,14 @@
-from modules.resource import Resource, Target
-
 class FeatureWrapper(object):
-    def __init__(self, feature, name=None, out=None):
+    def __init__(self, feature, out=None):
         self.feature = feature
-        self.name = feature.__name__ if name is None else name
         self.in_ = feature.IN
         self.out = feature.OUT if out is None else out
-        self.entities = {}
+        self.name = feature.__name__
 
-    def instantiate(self, dependencies, requested_resources, resources):
+    def instantiate(self, name, dependencies, requested_resources, resources):
         if len(self.out) == 0:
-            raise RuntimeError('You must specify output types for %s'%self.name)
-        feature_name = (self.name, requested_resources)
-        if issubclass(self.feature, Target):
-            name, out, fname = resources['target']
-            feature = self.feature(feature_name, dependencies, fname)
-        elif issubclass(self.feature, Resource):
-            name, out, fname = resources[self.name]
-            feature = self.feature(feature_name, dependencies, fname)
-        else:
-            feature = self.feature(feature_name, dependencies)
+            raise RuntimeError('You must specify output types for %s'%type(self.feature).__name__)
+        feature = self.feature(name, dependencies)
         feature.in_ = self.in_
         feature.out = self.out
         return feature
