e3ca5397f642c1c1ea9c83a625e21555cad22d06
Liam Childs <liam_childs@hotmail.com>
2014-06-11 20:33:19 +0200

    added various variation features

--- features/variation.py
+++ features/variation.py
@@ -1,41 +1,84 @@
+from collections import namedtuple
 from lhc.binf.genetic_code import GeneticCodes
+from lhc.binf.sequence import revcmp
 from modules.feature import Feature
 
-class CodingSequenceMutation(Feature):
+CodingVariant = namedtuple('CodingVariant', ('pos', 'ref', 'alt'))
+
+class VariantType(Feature):
     
-    IN = ['gene_position', 'variant']
-    OUT = ['coding_sequence_mutation']
+    IN = ['gene_model', 'amino_acid_variant']
+    OUT = ['variant_type']
     
-    def calculate(self, gene_position, variant):
-        if gene_position is None:
+    def calculate(self, gene_model, amino_acid_variant):
+        if gene_model is None:
+            return 'intergenic'
+        elif amino_acid_variant is None:
+            return 'intronic'
+        elif all(amino_acid_variant.ref == alt for alt in amino_acid_variant.alt):
+            return 'synonymous'
+        return 'non-synonymous'
+
+class CodingVariation(Feature):
+    
+    IN = ['gene_model', 'variant']
+    OUT = ['coding_variant']
+    
+    def calculate(self, gene_model, variant):
+        if gene_model is None:
             return None
-        return (variant.ref, gene_position, variant.alt.split(','))
+        coding_position = gene_model.transcripts.values()[0].getRelPos(variant.pos)
+        if coding_position is None:
+            return None
+        ref = variant.ref
+        alt = variant.alt.split(',')
+        if gene_model.ivl.strand == '-':
+            ref = revcmp(ref)
+            alt = map(revcmp, alt)
+        return CodingVariant(coding_position, ref, alt)
     
     def format(self, entity):
-        return ','.join('c.%s%s>%s'%(entity[1] + 1, entity[0], alt) for alt in entity[2])
+        return ','.join('c.%s%s>%s'%(entity.pos + 1, entity.ref, alt) for alt in entity.alt)
 
-class AminoAcidMutation(Feature):
+CodonVariant = namedtuple('CodonVariant', ['pos', 'ref', 'alt'])
+
+class CodonVariation(Feature):
     
-    IN = ['coding_sequence', 'gene_position', 'variant']
-    OUT = ['amino_acid_mutation']
+    IN = ['coding_variant', 'coding_sequence']
+    OUT = ['codon_variant']
+    
+    def calculate(self, coding_variant, coding_sequence):
+        if coding_variant is None:
+            return None
+        codon_pos = coding_variant.pos % 3
+        pos = coding_variant.pos - codon_pos
+        ref = coding_sequence[pos:pos + 3]
+        codon = list(ref)
+        alts = []
+        for alt in coding_variant.alt:
+            codon[codon_pos] = alt
+            alts.append(''.join(codon))
+        return CodonVariant(pos, ref, alts)
+    
+    def format(self, entity):
+        return ','.join('%s%s%s'%(entity.pos, entity.ref, alt) for alt in entity.alt)
+
+AminoAcidVariant = namedtuple('AminoAcidVariant', ('pos', 'ref', 'alt'))
+
+class AminoAcidVariation(Feature):
+    
+    IN = ['codon_variant']
+    OUT = ['amino_acid_variant']
     
     def init(self):
         self.gc = GeneticCodes().getCode(1)
 
-    def calculate(self, coding_sequence, gene_position, variant):
-        if gene_position is None:
+    def calculate(self, codon_variant):
+        if codon_variant is None:
             return None
-        position_in_codon = gene_position % 3
-        codon_position_in_coding_sequence = gene_position - position_in_codon
-        position_in_amino_acid_sequence = codon_position_in_coding_sequence / 3
-        codon = list(coding_sequence[codon_position_in_coding_sequence:codon_position_in_coding_sequence + 3])
-        aa1 = self.gc.translate(codon)
-        aa2 = []
-        for alt in variant.alt.split(','):
-            codon[position_in_codon] = alt
-            aa2.append(self.gc.translate(codon))
-        return (position_in_amino_acid_sequence, aa1, aa2)
+        return AminoAcidVariant(codon_variant.pos / 3,
+            self.gc.translate(codon_variant.ref),
+            [self.gc.translate(alt) for alt in codon_variant.alt])
     
     def format(self, entity):
-        return ','.join('%s%s%s'%(entity[1], entity[0] + 1, aa2) for aa2 in entity[2])
-        
+        return ','.join('%s%s%s'%(entity.ref, entity.pos + 1, alt) for alt in entity.alt)
