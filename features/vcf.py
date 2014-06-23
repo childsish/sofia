675e77a0b82e56b40eabde55424a00e1b051a74e
Liam Childs <liam_childs@hotmail.com>
2014-06-23 09:13:47 +0200

    added new vcf features

--- features/vcf.py
+++ features/vcf.py
@@ -5,8 +5,24 @@ class AccessVcfByPosition(Feature):
     IN = ['vcf', 'genomic_position']
     OUT = ['variant', 'genomic_position']
 
-    def calculate(self, genomic_position):
-        return self.data[genomic_position]
+    def calculate(self, vcf, genomic_position):
+        return vcf[genomic_position]
+
+class AccessVcfByInterval(Feature):
+    
+    IN = ['vcf', 'genomic_interval']
+    OUT = ['variant', 'genomic_position']
+    
+    def calculate(self, vcf, genomic_interval):
+        return vcf[genomic_interval]
+
+class AccessVcfByGeneModel(Feature):
+    
+    IN = ['vcf', 'gene_model']
+    OUT = ['variant', 'genomic_position']
+    
+    def calculate(self, vcf, gene_model):
+        return vcf[gene_model.ivl]
 
 class MatchVcf(Feature):
     
@@ -16,10 +32,18 @@ class MatchVcf(Feature):
     def calculate(self, variant):
         return variant is not None
 
-class CountVariantSamples(Feature):
+class VariantSampleCount(Feature):
+    
+    IN = ['variant']
+    OUT = ['variant_sample_count']
+    
+    def calculate(self, variant):
+        return sum(v['GT'] != '0/0' for v in variant.samples.itervalues())
+
+class VariantCount(Feature):
     
     IN = ['variant']
     OUT = ['variant_count']
     
     def calculate(self, variant):
-        return sum(v['GT'] != '0/0' for v in variant.samples.itervalues())
+        return len(variant)
