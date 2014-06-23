7eda33509009a3c3af9866b3827811b83fc7562f
Liam Childs <liam_childs@hotmail.com>
2014-06-23 10:41:50 +0200

    updated omim to have iterator and set

--- lhc/file_format/omim.py
+++ lhc/file_format/omim.py
@@ -1,17 +1,5 @@
+from omim_.iterator import OmimIterator
+
 def iterOmim(fname):
-    infile = open(fname)
-    infile.readline()
-    c_field = None
-    rec = {}
-    for line in infile:
-        if line.startswith('*RECORD*'):
-            for k in rec:
-                rec[k] = ''.join(rec[k]).strip()
-            yield rec['NO'], rec
-            rec = {}
-        elif line.startswith('*FIELD*'):
-            c_field = line.strip().split()[1]
-            rec[c_field] = []
-        else:
-            rec[c_field].append(line)
-    infile.close()
+    return OmimIterator(fname)
+
