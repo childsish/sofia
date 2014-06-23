ce9cf8e4d60e1e987071d6a418559b301d23d39b
Liam Childs <liam_childs@hotmail.com>
2014-06-23 09:13:20 +0200

    fasta access handles None

--- features/access_fasta.py
+++ features/access_fasta.py
@@ -6,11 +6,15 @@ class AccessFastaByModel(Feature):
     OUT = ['nucleotide_sequence']
 
     def calculate(self, fasta, gene_model):
+        if gene_model is None:
+            return None
         return gene_model.transcripts.values()[0].getSubSeq(fasta)
 
 class AccessFastaByHeader(Feature):
     
     IN = ['fasta', 'header']
 
-    def calculate(self, fasta, gene_model):
-        return fasta[gene_model]
+    def calculate(self, fasta, header):
+        if header is None:
+            return None
+        return fasta[header]
