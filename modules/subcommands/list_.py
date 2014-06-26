9f4c74d8a94c91182004357a3b76e704e923adda
Liam Childs <liam_childs@hotmail.com>
2014-06-26 15:48:19 +0200

    added list command

--- modules/subcommands/list_.py
+++ modules/subcommands/list_.py
@@ -1,8 +1,9 @@
 import argparse
 import os
 
-from common import loadPlugins, getProgramDirectory
+from common import getProgramDirectory
 from modules.feature import Feature
+from lhc.tools import loadPlugins
 
 def main():
     parser = getParser()
@@ -15,6 +16,7 @@ def getParser():
     return parser
 
 def defineParser(parser):
+    parser.add_argument('-v', '--verbose')
     parser.set_defaults(func=list_)
 
 def list_(args):
@@ -23,13 +25,16 @@ def list_(args):
     feature_types = loadPlugins(feature_dir, Feature)
     
     print '\nAvailable features:\n===================\n'
-    for name, type in sorted(feature_types.iteritems(), key=lambda x: x[1].__name__):
+    for name, type in sorted(feature_types.iteritems(), key=lambda x: x[0]):
         if name in ('resource', 'dynamic_resource', 'static_resource'):
             continue
-        print '%s (%s):\n\t%s'%(type.__name__, type.NAME, ', '.join(type.RESOURCES))
-        if hasattr(type, 'DESCRIPTION'):
-            print '\t%s'%type.DESCRIPTION
-        print
+        print name
+        if args.verbose:
+            print 'Input\n-----\n%s\n'%', '.join(type.IN)
+            print 'Output\n------\n%s\n'%', '.join(type.OUT)
+            if hasattr(type, 'DESCRIPTION'):
+                print 'Description\n-----------\n%s'%type.DESCRIPTION
+            print
 
 if __name__ == '__main__':
     import sys
