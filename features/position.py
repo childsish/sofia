d9b043619f1f4e1f728c4728e05d3a96fb96960e
Liam Childs <liam_childs@hotmail.com>
2014-07-04 16:31:34 +0200

    added many new position features

--- features/position.py
+++ features/position.py
@@ -16,5 +16,53 @@ class Position(Feature):
     def calculate(self, genomic_position):
         return genomic_position.pos
 
-    def format(self, genomic_position):
-        return str(genomic_position + 1)
+    def format(self, position):
+        return str(position + 1)
+
+class Quality(Feature):
+
+    IN = ['variant']
+    OUT = ['quality']
+
+    def calculate(self, variant):
+        return variant.qual
+
+    def format(self, quality):
+        if isinstance(quality, basestring):
+            return quality
+        return '%.2f'%quality
+
+class Reference(Feature):
+
+    IN = ['variant']
+    OUT = ['reference']
+
+    def calculate(self, variant):
+        return variant.ref
+
+class Alternative(Feature):
+
+    IN = ['variant']
+    OUT = ['alternative']
+
+    def calculate(self, variant):
+        return variant.alt
+
+class ReferenceCount(Feature):
+
+    IN = ['variant']
+    OUT = ['reference_count']
+
+    def calculate(self, variant):
+        sample = variant.samples.values()[0]
+        return sample['RO'] if 'RO' in sample else 'NA'
+
+class AlternativeCount(Feature):
+
+    IN = ['variant']
+    OUT = ['alternative_count']
+
+    def calculate(self, variant):
+        sample = variant.samples.values()[0]
+        return sample['AO'] if 'AO' in sample else 'NA'
+
