193c361d7812a977eb6fed7eb7b53fc6c66f837b
Liam Childs <liam_childs@hotmail.com>
2014-07-10 21:53:36 +0200

    added __str__ to Resource

--- ebias/resource.py
+++ ebias/resource.py
@@ -3,3 +3,6 @@ class Resource(object):
         self.fname = fname
         self.type = type
         self.name = fname if name is None else name
+
+    def __str__(self):
+        return '%s;type=%s;%s'%(self.fname, self.type, self.name)
