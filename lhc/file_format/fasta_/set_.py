950c2f93f2e182dbe694af29b868d6cfce5afc9f
Liam Childs <liam_childs@hotmail.com>
2014-08-13 15:23:33 +0200

    removed fasta set caching
    only makes sense for indexed fasta files

--- lhc/file_format/fasta_/set_.py
+++ lhc/file_format/fasta_/set_.py
@@ -1,21 +1,12 @@
 class FastaSet(object):
     def __init__(self, iterator):
         self.data = dict(iterator)
-        self.prv_key = None
-        self.prv_value = None
 
     def __getitem__(self, key):
-        if self.prv_key == key:
-            return self.prv_value
-        else:
-            self.prv_key = key
-        
         if isinstance(key, basestring):
-            self.prv_value = self.data[key]
+            return self.data[key]
         elif hasattr(key, 'chr') and hasattr(key, 'pos'):
-            self.prv_value =  self.data[key.chr][key.pos]
+            return self.data[key.chr][key.pos]
         elif hasattr(key, 'chr') and hasattr(key, 'start') and hasattr(key, 'stop'):
-            self.prv_value =  self.data[key.chr][key.start:key.stop]
-        else:
-            raise NotImplementedError('Random access not implemented for %s'%type(key))
-        return self.prv_value
+            return self.data[key.chr][key.start:key.stop]
+        raise NotImplementedError('Fasta set random access not implemented for %s'%type(key))
