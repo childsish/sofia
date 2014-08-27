69ed52ba75ae41ae84e6d920b346069de17c8519
Liam Childs <liam_childs@hotmail.com>
2014-08-27 16:12:25 +0200

    added major transcript name feature

--- features/gene_model.py
+++ features/gene_model.py
@@ -10,13 +10,27 @@ class GeneName(Feature):
             return ''
         return gene_model.name
 
+class MajorTranscriptName(Feature):
+    
+    IN = ['gene_model']
+    OUT = ['major_transcript_name']
+
+    def calculate(self, gene_model):
+        if gene_model is None:
+            return None
+        major_transcript = sorted(gene_model.transcripts.itervalues(),\
+            key=lambda x:len(x))[-1]
+        return major_transcript.name
+
 class GenePosition(Feature):
     
     IN = ['genomic_position', 'gene_model']
     OUT = ['gene_position']
 
     def calculate(self, genomic_position, gene_model):
-        return gene_model.transcripts.values()[0].getRelPos(genomic_position.pos)
+        major_transcript = sorted(gene_model.transcripts.itervalues(),\
+            key=lambda x:len(x))[-1]
+        return major_transcript.getRelPos(genomic_position.pos)
 
     def format(self, gene_position):
         return '' if gene_position is None else str(gene_position + 1)
