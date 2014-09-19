dd97d287b4fda10bc9532f418c1aaf360a84e67d
Liam Childs <liam_childs@hotmail.com>
2014-09-19 09:00:11 +0200

    added fasta index support

--- features/fasta.py
+++ features/fasta.py
@@ -1,3 +1,5 @@
+import os
+
 from ebias.resource import Resource, Target
 
 from lhc.file_format.fasta_.set_ import FastaSet as FastaSetParser
@@ -6,26 +8,26 @@ from lhc.file_format.fasta_.index import IndexedFastaFile
 
 class FastaChromosomeSequenceSet(Resource):
     
-    EXT = ['.fasta', '.fasta.gz', '.fasta.idx']
+    EXT = ['.fasta', '.fasta.gz']
     TYPE = 'chromosome_sequence'
     PARSER = FastaSetParser
     OUT = ['chromosome_sequence_set']
     
     def init(self, **kwargs):
         fname = self.getFilename()
-        self.parser = IndexedFastaFile(fname) if fname.endswith('.fasta.idx')\
+        self.parser = IndexedFastaFile(fname) if os.path.exists('%s.fai'%fname)\
             else FastaSetParser(FastaIteratorParser(fname))
 
 class FastaCodingSequenceSet(Resource):
     
-    EXT = ['.fasta', '.fasta.gz', '.fasta.idx']
+    EXT = ['.fasta', '.fasta.gz']
     TYPE = 'coding_sequence'
     PARSER = FastaSetParser
     OUT = ['coding_sequence_set']
     
     def init(self, **kwargs):
         fname = self.getFilename()
-        self.parser = IndexedFastaFile(fname) if fname.endswith('.fasta.idx')\
+        self.parser = IndexedFastaFile(fname) if os.path.exists('%s.fai'%fname)\
             else FastaSetParser(FastaIteratorParser(fname))
 
 class FastaCodingSequenceIterator(Target):
@@ -38,14 +40,14 @@ class FastaCodingSequenceIterator(Target):
 
 class FastaProteinSequenceSet(Resource):
     
-    EXT = ['.fasta', '.fasta.gz', '.fasta.idx']
+    EXT = ['.fasta', '.fasta.gz']
     TYPE = 'protein_sequence'
     PARSER = FastaSetParser
     OUT = ['protein_sequence_set']
     
     def init(self, **kwargs):
         fname = self.getFilename()
-        self.parser = IndexedFastaFile(fname) if fname.endswith('.fasta.idx')\
+        self.parser = IndexedFastaFile(fname) if os.path.exists('%s.fai'%fname)\
             else FastaSetParser(FastaIteratorParser(fname))
 
 class FastaProteinSequenceIterator(Target):
