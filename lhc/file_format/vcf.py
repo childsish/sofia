eb9172f696bf42e8970977d258d1845feb40f591
Liam Childs <liam_childs@hotmail.com>
2014-09-29 17:57:33 +0200

    updated vcf merger to handle bam files

--- lhc/file_format/vcf.py
+++ lhc/file_format/vcf.py
@@ -8,17 +8,20 @@ from vcf_.iterator import VcfIterator
 def iterEntries(fname):
     return VcfIterator(fname)
 
-def merge(fnames, quality=50.0, out=None):
+def merge(fnames, quality=50.0, out=None, bams=[]):
     import sys
     
     fnames = reduce(add, (glob.glob(fname) for fname in fnames))
     out = sys.stdout if out is None else open(out, 'w')
-    merger = VcfMerger(fnames, quality)
+    merger = VcfMerger(fnames, quality, bams=bams)
     for key, values in merger.hdrs.iteritems():
         for value in values:
             out.write('%s=%s\n'%(key, value))
     out.write('#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t' + '\t'.join(merger.sample_names) + '\n')
     for entry in merger:
+        format = sorted(key for key in entry.samples.itervalues().next().keys()\
+            if key != '.')
+        sample = entry.samples
         out.write('%s\t%d\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n'%(\
             entry.chr,
             entry.pos + 1,
@@ -28,9 +31,10 @@ def merge(fnames, quality=50.0, out=None):
             entry.qual,
             entry.filter,
             entry.info,
-            ':'.join(entry.samples.values()[0].iterkeys()),
-            '\t'.join(':'.join(sample.itervalues())\
-                for sample in entry.samples.itervalues())
+            ':'.join(format),
+            '\t'.join('.' if '.' in sample else
+                ':'.join(entry.samples[sample][f] for f in format)\
+                for sample in merger.sample_names)
         ))
     out.close()
 
@@ -45,11 +49,11 @@ def getArgumentParser():
     
     merge_parser = subparsers.add_parser('merge')
     merge_parser.add_argument('inputs', nargs='+', metavar='FILE')
+    merge_parser.add_argument('-b', '--bams', nargs='+')
     merge_parser.add_argument('-q', '--quality', type=float)
     merge_parser.add_argument('-o', '--output')
     merge_parser.set_defaults(func=lambda args: merge(args.inputs,
-                                                      args.quality,
-                                                      args.output))
+        args.quality, args.output, args.bams))
     
     return parser
 
--- lhc/file_format/vcf_/merger.py
+++ lhc/file_format/vcf_/merger.py
@@ -1,7 +1,7 @@
 import gzip
 import re
 
-from collections import OrderedDict, defaultdict
+from collections import OrderedDict, defaultdict, Counter
 from itertools import count, izip
 from lhc.binf.identifier import Chromosome
 from lhc.collection.sorted_dict import SortedDict
@@ -12,7 +12,7 @@ class VcfMerger(object):
     
     CHR_REGX = re.compile('\d+$|X$|Y$|M$')
     
-    def __init__(self, fnames, quality=0, depth=0):
+    def __init__(self, fnames, quality=0, depth=0, bams=[]):
         self.fnames = fnames
         self.quality = quality
         self.depth = depth
@@ -21,6 +21,15 @@ class VcfMerger(object):
         self.hdrs = self._mergeHeaders(hdrs)
         self.sample_names = self.hdrs['##SAMPLES']
         del self.hdrs['##SAMPLES']
+        if len(bams) == 0:
+            self.bams = []
+            self.sample_to_bam = {}
+        else:
+            import pysam
+            self.bams = [pysam.Samfile(bam) for bam in bams]
+            self.sample_to_bam = {bam.header['RG'][0]['SM']: bam\
+                for bam in self.bams}
+            print self.sample_to_bam
 
     def __iter__(self):
         """ Iterate through merged vcf lines.
@@ -33,44 +42,73 @@ class VcfMerger(object):
         while len(sorted_tops) > 0:
             key, idxs = sorted_tops.popLowest()
             
-            refs = set(tops[idx].ref for idx in idxs)
-            longest_ref = sorted(refs, key=lambda x:len(x))[-1]
-            merged_alleles = set()
-            for idx in idxs:
-                top = list(tops[idx])
-                ref = top[VcfIterator.REF]
-                alts = [alt + longest_ref[len(ref):]\
-                    for alt in top[VcfIterator.ALT].split(',')]
-                merged_alleles.update(alts)
-                top[VcfIterator.REF] = longest_ref
-                top[VcfIterator.ALT] = alts
-                tops[idx] = Variant(*top)
-            merged_alleles = [longest_ref] + sorted(merged_alleles)
-            entry = list(tops[idxs[0]])
-            entry[VcfIterator.ALT] = ','.join(merged_alleles[1:])
-            entry[VcfIterator.QUAL] = min(tops[idx].qual for idx in idxs)
-            entry[VcfIterator.INFO] = '.'
-            
-            ao = ','.join((len(merged_alleles) - 1) * ['0'])
-            samples = OrderedDict([(name, OrderedDict([('GT', '0/0'),
-                                          ('GQ', '0'),
-                                          ('RO', '0'),
-                                          ('AO', ao),
-                                          ('Q', '0')]))
-                for name in self.sample_names])
+            ref = sorted((tops[idx].ref for idx in idxs),\
+                key=lambda x:len(x))[-1]
+            alt = set()
             for idx in idxs:
                 top = tops[idx]
-                alleles = [longest_ref] + top.alt
-                for sample_name, sample_data in top.samples.iteritems():
-                    samples[sample_name].update(sample_data)
-                    samples[sample_name]['Q'] = str(top.qual)
+                alt.update(alt + ref[len(top.ref):]\
+                    for alt in top.alt.split(','))
+            alt = sorted(alt)
+            
+            sample_to_top = {}
+            for idx in idxs:
+                top = tops[idx]
+                for sample in top.samples:
+                    sample_to_top[sample] = top
+
+            format_ = {}
+            samples = {}
+            for sample_name in self.sample_names:
+                if sample_name in sample_to_top:
+                    top = sample_to_top[sample_name]
+                    sample_data = top.samples[sample_name]
+                    if 'Q' not in format_: format_['Q'] = ''
+                    if 'GT' not in format_: format_['GT'] = '0/0'
+                    samples[sample_name] = {'Q': '' if top.qual == '.' else\
+                        '%.4f'%top.qual}
+                    samples[sample_name]['GT'] =\
+                        self._getGT(sample_data['GT'], top.alt, alt)
+                    if 'GQ' in sample_data:
+                        if 'GQ' not in format_: format_['GQ'] = ''
+                        samples[sample_name]['GQ'] = sample_data['GQ']
                     if 'RO' in sample_data:
-                        merged_counts = self._mergeCounts(sample_data,
-                            merged_alleles, alleles)
+                        if 'RO' not in format_: format_['RO'] = '0'
+                        if 'AO' not in format_: format_['AO'] =\
+                            ','.join('0' * len(alt))
                         samples[sample_name]['RO'] = sample_data['RO']
-                        samples[sample_name]['AO'] = ','.join(merged_counts[1:])
-            entry[8] = samples
-            yield Variant(*entry)
+                        samples[sample_name]['AO'] =\
+                            self._getAO(sample_data['AO'], top.alt, alt)
+                elif sample_name in self.sample_to_bam:
+                    if 'GT' not in format_: format_['GT'] = '0/0'
+                    if 'RO' not in format_: format_['RO'] = '0'
+                    if 'AO' not in format_: format_['AO'] =\
+                        ','.join('0' * len(alt))
+                    ro, ao = self._getDepth(sample_name,
+                        tops[idxs[0]].chr,
+                        tops[idxs[0]].pos,
+                        ref,
+                        alt)
+                    samples[sample_name] = {'.': '.'} if ro is None\
+                        else {'RO': ro, 'AO': ao}
+                else:
+                    samples[sample_name] = {'.': '.'}
+            
+            for sample in samples.itervalues():
+                for fmt, default in format_.iteritems():
+                    if fmt not in sample:
+                        sample[fmt] = default
+
+            yield Variant(tops[idxs[0]].chr,
+                tops[idxs[0]].pos,
+                tops[idxs[0]].id,
+                ref,
+                ','.join(alt),
+                min(tops[idx].qual for idx in idxs),
+                '.',
+                '.',
+                samples)
+
             for idx in idxs:
                 try:
                     tops[idx] = self._nextLine(idx)
@@ -106,20 +144,45 @@ class VcfMerger(object):
                 values.update((value, None) for value in hdr[k])
             res[k] = list(values)
         return res
-
-    def _mergeCounts(self, sample_data, merged_alleles, alleles):
-        merged_counts = len(merged_alleles) * ['0']
-        counts = [sample_data['RO']] + sample_data['AO'].split(',')
-        for allele, count in izip(alleles, counts):
-            merged_counts[merged_alleles.index(allele)] = count
-        if sample_data['GT'] != '0/0':
-            try:
-                a1, a2 = map(int, sample_data['GT'].split('/'))
-            except Exception, e:
-                print self.iterators[idx].fname
-                raise e
-            sample_data['GT'] = '/'.join(map(str,\
-                (merged_alleles.index(alleles[a1]),\
-                 merged_alleles.index(alleles[a2]))))
-        return merged_counts
+    
+    def _getGT(self, gt, old_alt, new_alt):
+        old_alt = old_alt.split(',')
+        a1, a2 = map(int, gt.split('/'))
+        if a1 != '0': a1 = str(new_alt.index(old_alt[a1 - 1]) + 1)
+        if a2 != '0': a2 = str(new_alt.index(old_alt[a2 - 1]) + 1)
+        return '%s/%s'%(a1, a2)
+    
+    def _getAO(self, ao, old_alt, new_alt):
+        res = {k: v for k, v in izip(old_alt.split(','), ao.split(','))}
+        return ','.join(res[a] if a in res else '0' for a in new_alt)
+    
+    def _getDepth(self, sample, chr, pos, ref, alt):
+        bam = self.sample_to_bam[sample]
+        ref_start = pos
+        ref_stop = pos + len(ref)
+        cnt = Counter()
+        for read in bam.fetch(chr, ref_start, ref_stop):
+            read_start, read_stop =\
+                self._getReadInterval(read, ref_start, ref_stop)
+            cnt[read.seq[read_start:read_stop]] += 1
+        if cnt[ref] == 0 and all(cnt[a] == 0 for a in alt):
+            return None, None
+        return str(cnt[ref]), ','.join(str(cnt[a]) for a in alt)
+    
+    def _getReadInterval(self, read, ref_start, ref_stop):
+        read_start = 0
+        read_stop = 0
+        ref_pos = 0
+        read_pos = read.qstart
+        for op, length in read.cigar:
+            read_ext = [1, 1, 0, 1, 1, 1, 1, 1, 1][op] * length
+            ref_ext = [1, 0, 1, 1, 1, 1, 1, 1, 1][op] * length
+            if read.pos + ref_pos + ref_ext > ref_start and read_start == 0:
+                read_start = read_pos + (ref_start - ref_pos - read.pos)
+            if read.pos + ref_pos + ref_ext > ref_stop:
+                read_stop = read_pos + (ref_stop - ref_pos - read.pos)
+                break
+            read_pos += read_ext
+            ref_pos += ref_ext
+        return read_start, read_stop
 
