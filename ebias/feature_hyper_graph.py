98c4d2c00703f58f0ab96a05d945ff5ab1066e72
Liam Childs <liam_childs@hotmail.com>
2014-10-07 22:06:10 +0200

    integrated entity converters

--- ebias/feature_hyper_graph.py
+++ ebias/feature_hyper_graph.py
@@ -6,15 +6,16 @@ from lhc.graph.hyper_graph import HyperGraph
 from error_manager import ERROR_MANAGER
 from feature_graph import FeatureGraph
 from feature_wrapper import FeatureWrapper
-from features import Resource, Target, Extractor
+from features import Resource, Target, Extractor, Converter
 
 class FeatureHyperGraph(object):
     """ A hyper graph of all the possible feature calculation pathways. """
-    def __init__(self):
+    def __init__(self, entity_graph):
         self.outs = defaultdict(set)
         self.ins = defaultdict(set)
         self.features = {}
         self.graph = HyperGraph()
+        self.entity_graph = entity_graph
     
     def __str__(self):
         """ Returns a dot formatted representation of the graph. """
@@ -23,7 +24,7 @@ class FeatureHyperGraph(object):
     def registerFeature(self, feature):
         """ Add a feature to the hyper graph. """
         def getEntityName(entity):
-            return ''.join(part.capitalize() for part in entity.split('_'))
+            return ''.join(part.capitalize() for part in entity.split('_')).replace('*', '')
         
         c_feature = feature.name
         self.features[c_feature] = feature
@@ -35,28 +36,74 @@ class FeatureHyperGraph(object):
             for in_origin in self.outs[in_]:
                 self.graph.addEdge(in_, c_feature, in_origin)
             for path in self.entity_graph.getAncestorPaths(in_):
+                if len(path) != 2:
+                    continue
+                extractor_name = 'Extract%sFrom%s'%\
+                        (getEntityName(path[-1]), getEntityName(path[0]))
                 extractor = FeatureWrapper(Extractor,
-                    'Extract%sFrom%s'%\
-                        (getEntityName(path[-1]), getEntityName(path[0])),
+                    extractor_name,
                     ins=[path[0]],
                     outs=[path[-1]],
                     kwargs={'path': path})
-                for in_origin in self.outs[path[0]]:
-                    self.registerFeature(extractor)
+                self.features[extractor_name] = extractor
+                self.graph.addVertex(extractor_name)
+                self.graph.addEdge(path[0], extractor_name)
+                for in_ in extractor.ins:
+                    self.ins[in_].add(extractor_name)
+                    for in_origin in self.outs[in_]:
+                        self.graph.addEdge(in_, extractor_name, in_origin)
+                self.outs[path[-1]].add(extractor_name)
+                for out_destination in self.ins[path[-1]]:
+                    self.graph.addEdge(path[-1], out_destination, extractor_name)
 
         for out in feature.outs:
             self.outs[out].add(c_feature)
             for out_destination in self.ins[out]:
                 self.graph.addEdge(out, out_destination, c_feature)
             for path in self.entity_graph.getDescendentPaths(out):
+                if len(path) != 2:
+                    continue
+                extractor_name = 'Extract%sFrom%s'%\
+                        (getEntityName(path[-1]), getEntityName(path[0]))
                 extractor = FeatureWrapper(Extractor,
-                    'Extract%sFrom%s'%\
-                        (getEntityName(path[-1]), getEntityName(path[0])),
+                    extractor_name,
                     ins=[path[0]],
                     outs=[path[-1]],
                     kwargs={'path': path})
+                self.features[extractor_name] = extractor
+                self.graph.addVertex(extractor_name)
+                self.graph.addEdge(path[0], extractor_name)
+                for in_ in extractor.ins:
+                    self.ins[in_].add(extractor_name)
+                    for in_origin in self.outs[in_]:
+                        self.graph.addEdge(in_, extractor_name, in_origin)
+                self.outs[path[-1]].add(extractor_name)
                 for out_destination in self.ins[path[-1]]:
-                    self.registerFeature(extractor)
+                    self.graph.addEdge(path[-1], out_destination, extractor_name)
+        
+        if feature.feature_class is not Converter and\
+                issubclass(feature.feature_class, Converter):
+            convertee = list(set(feature.ins) & set(feature.outs))[0]
+            id_map = list(set(feature.ins) - set(feature.outs))[0]
+            for path in self.entity_graph.getAncestorPaths(convertee):
+                converter_name = 'Convert%sIn%s'%\
+                        (getEntityName(path[-1]), getEntityName(path[0]))
+                converter = FeatureWrapper(Converter,
+                    converter_name,
+                    ins=[path[0], id_map],
+                    outs=[path[0]],
+                    kwargs={'path': path})
+                self.features[converter_name] = converter
+                self.graph.addVertex(converter_name)
+                self.graph.addEdge(path[0], converter_name)
+                for in_ in converter.ins:
+                    self.ins[in_].add(converter_name)
+                    for in_origin in self.outs[in_]:
+                        self.graph.addEdge(in_, converter_name, in_origin)
+                self.outs[path[0]].add(converter_name)
+                for out_destination in self.ins[path[0]]:
+                    self.graph.addEdge(path[0], out_destination, converter_name)
+
 
     def iterFeatureGraphs(self, feature_name, requested_feature, resources, visited=None):
         """ Find all possible resolutions for the given feature_name. """
