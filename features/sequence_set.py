fcaece0f441ea8be71a3d08ab4c4e072b5e1b44b
Liam Childs <liam_childs@hotmail.com>
2014-10-14 10:40:00 +0200

    added get 1000nt downstream

--- features/sequence_set.py
+++ features/sequence_set.py
@@ -1,5 +1,7 @@
 from ebias.features import Feature
 
+from lhc.binf.sequence import revcmp
+
 class GetCodingSequenceByGeneModel(Feature):
 
     IN = ['chromosome_sequence_set', 'major_transcript']
@@ -9,3 +11,17 @@ class GetCodingSequenceByGeneModel(Feature):
         if major_transcript is None:
             return None
         return major_transcript.getSubSeq(chromosome_sequence_set)#, type='CDS')
+
+class GetDownstream1000(Feature):
+
+    IN = ['chromosome_sequence_set', 'genomic_position', 'major_transcript']
+    OUT = ['downstream_1000']
+    
+    def calculate(self, chromosome_sequence_set, genomic_position, major_transcript):
+        chr = genomic_position['chromosome_id']
+        pos = genomic_position['chromosome_pos']
+        strand = major_transcript.ivl.strand
+        start = pos if strand == '+' else pos - 1000
+        stop = pos if strand == '-' else pos + 1000
+        seq = chromosome_sequence_set.getInterval(chr, start, stop)
+        return seq if strand == '+' else revcmp(seq)
