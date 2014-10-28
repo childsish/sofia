94914b2f9410dcbd49ffbd3781e721ea6a29f13b
Liam Childs <liam_childs@hotmail.com>
2014-10-28 15:33:02 +0100

    fixed feature wrapper ins definition

--- ebias/feature_wrapper.py
+++ ebias/feature_wrapper.py
@@ -1,11 +1,11 @@
-from itertools import izip
 from ebias.entity import Entity
 
 class FeatureWrapper(object):
     def __init__(self, feature_class, name=None, ins=None, outs=None, kwargs={}):
         self.feature_class = feature_class
         self.name = feature_class.__name__ if name is None else name
-        self.ins = feature_class.IN if ins is None else ins
+        self.ins = {in_: Entity(in_) for in_ in feature_class.IN}\
+            if ins is None else ins
         self.outs = {out: Entity(out) for out in feature_class.OUT}\
             if outs is None else outs
         self.kwargs = kwargs
@@ -13,7 +13,7 @@ class FeatureWrapper(object):
     def __call__(self, resources=None, dependencies=None, kwargs={}, ins=None, outs=None):
         tmp_kwargs = self.kwargs.copy()
         tmp_kwargs.update(kwargs)
-        #ins = dict(izip(self.ins, self.feature_class.IN))
+        ins = self.ins if ins is None else ins
         outs = self.outs if outs is None else outs
         return self.feature_class(resources, dependencies, tmp_kwargs, ins, outs)
 
