f1abc8d24393b5cbaa0cd1148d7a343009656823
Liam Childs <liam_childs@hotmail.com>
2014-10-28 16:40:43 +0100

    fixed id map

--- lhc/file_format/id_map.py
+++ lhc/file_format/id_map.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from operator import or_
+from itertools import izip
 
 class IdMap(object):
     """ An map for equivalent ids.
@@ -7,27 +8,15 @@ class IdMap(object):
     Will create a map from one column of a tab delimited file to another.
     Always requires a header line.
     """
-    def __init__(self, fname, fr=None, to=None, id_map=None):
+    def __init__(self, fname):
         fhndl = open(fname)
-        hdrs = fhndl.next().strip().split('\t')
-        fr_idx = 0 if fr is None else hdrs.index(fr)
-        to_idx = 1 if to is None else hdrs.index(to)
-        self.map = defaultdict(set)
+        self.hdrs = fhndl.next().strip().split('\t')
+        self.map = {}
         for line in fhndl:
             parts = line.strip().split('\t')
-            try:
-                self.map[parts[fr_idx]].add(parts[to_idx])
-            except IndexError:
-                pass
+            for entry in izip(self.hdrs, parts):
+                self.map[entry] = dict(izip(self.hdrs, parts))
         fhndl.close()
-        self.id_map = id_map
     
-    def __getitem__(self, key):
-        if self.id_map is not None:
-            key = self.id_map[key]
-        if isinstance(key, basestring):
-            return self.map[key]
-        res = reduce(or_, (self.map[k] for k in key), set())
-        if len(res) == 1:
-            return list(res)[0]
-        return list(res)
+    def mapIdentifier(self, fr, to, key):
+        return self.map[(fr, key)][to]
