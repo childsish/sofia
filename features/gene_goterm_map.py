364bd0f153c82a232424961e10a6aa6cb7fa05b4
Liam Childs <liam_childs@hotmail.com>
2014-11-05 10:32:36 +0100

    refactored annotation code

---
+++ features/gene_goterm_map.py
@@ -0,0 +1,34 @@
+from ebias.features import Feature, Resource
+
+from collections import defaultdict
+
+class GeneGotermMap(Resource):
+
+    EXT = ['.txt']
+    OUT = ['gene_goterm_map']
+
+    def init(self):
+        fhndl = open(self.getFilename())
+        fhndl.next()
+        self.parser = defaultdict(set)
+        for line in fhndl:
+            parts = line.strip().split('\t')
+            self.parser[parts[0]].add((parts[1], parts[2]))
+        fhndl.close()
+
+class GetGotermByGene(Feature):
+    
+    IN = ['gene_id', 'gene_goterm_map']
+    OUT = ['goterm']
+    
+    def init(self, domain=None):
+        self.domain = domain
+    
+    def calculate(self, gene_id, gene_goterm_map):
+        if self.domain is None:
+            return set(goterm for goterm, domain in gene_goterm_map[gene_id]\
+                if domain == self.domain)
+        return set(goterm for goterm, domain in gene_goterm_map[gene_id])
+    
+    def format(self, goterm):
+        return ','.join(sorted(goterm))
--- features/gene_pathway_map.py
+++ features/gene_pathway_map.py
@@ -1,12 +1,12 @@
-from ebias.features import Resource
+from ebias.features import Feature, Resource
 
 from collections import defaultdict
 
 class GenePathwayMap(Resource):
-    
+
     EXT = ['.txt']
     OUT = ['gene_pathway_map']
-    
+
     def init(self):
         fhndl = open(self.getFilename())
         fhndl.next()
@@ -15,3 +15,14 @@ class GenePathwayMap(Resource):
             gene_id, pathway_id = line.strip().split('\t')
             self.parser[gene_id].add(pathway_id)
         fhndl.close()
+
+class GetPathwayByGene(Feature):
+    
+    IN = ['gene_id', 'gene_pathway_map']
+    OUT = ['pathway_id']
+    
+    def calculate(self, gene_id, gene_pathway_map):
+        return gene_pathway_map[gene_id]
+    
+    def format(self, pathway_id):
+        return ','.join(sorted(pathway_id))
