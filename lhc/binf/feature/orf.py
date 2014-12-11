4587e3daacb687dab806ea0cf0c9d88c5b989cb8
Liam Childs <liam_childs@hotmail.com>
2014-12-11 22:39:38 +0100

    transferred orf feature to sofia

--- lhc/binf/feature/orf.py
+++
@@ -1,9 +0,0 @@
-from feature import Feature, Transform
-from lhc.binf.orf import findORFs
-
-class ORFNumber(Feature):
-    def __init__(self):
-        super(ORFNumber, self).__init__()
-
-    def calculate(self, seq, dep_res):
-        return {'norf': len(findORFs(seq))}
