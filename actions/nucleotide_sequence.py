cb84fa397ebab5a53f6933b54537a6930d8cd181
Liam Childs <liam_childs@hotmail.com>
2014-12-11 21:28:31 +0100

    removed redundant code

--- actions/nucleotide_sequence.py
+++ actions/nucleotide_sequence.py
@@ -29,20 +29,3 @@ class NucleotideSkew(Action):
         at_skew = (counts['a'] - counts['t']) / float(counts['a'] + counts['t'])
         gc_skew = (counts['g'] - counts['c']) / float(counts['g'] + counts['c'])
         return {'at_skew': at_skew, 'gc_skew': gc_skew}
-
-
-class OpenReadingFrames(Action):
-
-    IN = ['nucleotide_sequence']
-    OUT = ['open_reading_frame']
-
-    def calculate(self, nucleotide_sequence):
-        stops = set(('taa', 'tag', 'tga'))
-        res = []
-        for i in xrange(len(nucleotide_sequence)):
-            if not nucleotide_sequence[i:i + 3] == 'atg':
-                continue
-            for j in xrange(i, len(nucleotide_sequence), 3):
-                if nucleotide_sequence[j:j + 3] in stops:
-                    res.append(nucleotide_sequence[i:j + 3])
-        return res
