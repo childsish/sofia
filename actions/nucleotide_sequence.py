f51f17b0de24a5674c0baa6be58aa4ea8139c06c
Liam Childs <liam_childs@hotmail.com>
2014-12-15 11:50:11 +0100

    removed redundant code

--- actions/nucleotide_sequence.py
+++ actions/nucleotide_sequence.py
@@ -17,15 +17,3 @@ class CalculateKNucleotideFrequency(Action):
 
     def format(self, nucleotide_frequency):
         return str(nucleotide_frequency)
-
-
-class NucleotideSkew(Action):
-
-    IN = ['nucleotide_sequence']
-    OUT = ['nucleotide_skew']
-
-    def calculate(self, nucleotide_sequence):
-        counts = Counter(nucleotide_sequence.lower())
-        at_skew = (counts['a'] - counts['t']) / float(counts['a'] + counts['t'])
-        gc_skew = (counts['g'] - counts['c']) / float(counts['g'] + counts['c'])
-        return {'at_skew': at_skew, 'gc_skew': gc_skew}
