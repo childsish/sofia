fa3f263eb74be5834dc20fbb3f4202dcb1f582c9
Liam Childs <liam_childs@hotmail.com>
2015-01-05 18:42:45 +0100

    fixed gtf iterator import

--- lhc/file_format/gtf_/iterator.py
+++ lhc/file_format/gtf_/iterator.py
@@ -1,7 +1,7 @@
 import gzip
 
-from binf.gene_model import Gene, Transcript, Exon
-from binf.genomic_coordinate import Interval
+from lhc.binf.gene_model import Gene, Transcript, Exon
+from lhc.binf.genomic_coordinate import Interval
 
 
 class GtfIterator(object):
