b7ea761f2409072b5ea2ba24079d4164ff82cb10
Liam Childs <liam_childs@hotmail.com>
2015-01-10 16:09:58 +0100

    fixed bam subset bugs

--- lhc/file_format/bam_/subset.py
+++ lhc/file_format/bam_/subset.py
@@ -12,7 +12,7 @@ def subset(args):
 def subset_bed(input, output, bed):
     from lhc.file_format.bed import iter_entries as iter_bed
     in_fhndl = pysam.AlignmentFile(input)
-    out_fhndl = pysam.AlignedFile(output, 'w', template=in_fhndl)
+    out_fhndl = pysam.AlignmentFile(output, 'wb', template=in_fhndl)
     for entry in iter_bed(bed):
         for read in in_fhndl.fetch(entry.chr, entry.start, entry.stop):
             out_fhndl.write(read)
@@ -23,7 +23,7 @@ def subset_bed(input, output, bed):
 def subset_random(input, output, proportion):
     raise NotImplementedError()
     in_fhndl = pysam.AlignmentFile(input)
-    out_fhndl = pysam.AlignedFile(output, 'w', template=in_fhndl)
+    out_fhndl = pysam.AlignedFile(output, 'wb', template=in_fhndl)
     in_fhndl.close()
     out_fhndl.close()
 
@@ -42,10 +42,10 @@ def define_parser(parser):
     group.add_argument('-b', '--bed',
                        help='Get reads that overlap with the ranges defined in a bed file.')
     group.add_argument('-r', '--random', type=float, default=0.1,
-                              help='Sample the given proportion of reads from the input.')
+                       help='Sample the given proportion of reads from the input.')
 
     add_arg = parser.add_argument
-    add_arg('-i', '--input', default='-',
+    add_arg('input', default='-',
             help='The input bam file.')
     add_arg('-o', '--output', default='-',
             help='The output bam file.')
