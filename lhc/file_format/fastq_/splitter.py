12c2e08580351d569e7b100b472ede6797cd0291
Liam Childs <liam_childs@hotmail.com>
2015-01-14 17:35:47 +0100

    converted fastq splitter to multiprocessing

--- lhc/file_format/fastq_/splitter.py
+++ lhc/file_format/fastq_/splitter.py
@@ -1,40 +1,55 @@
 import argparse
+import multiprocessing
+import os
+import sys
 
 from iterator import FastqEntryIterator
+from lhc.argparse import OpenReadableFile
 from lhc.binf.alignment.local_aligner import LocalAligner
-from lhc.file_format.fasta import FastaEntryIterator, FastaSet
-
-
-class FastqBarcodeSplitter(object):
-    def __init__(self, score, gap_penalty):
-        self.aligner = LocalAligner(gap_penalty=gap_penalty)
-        self.score = score
-
-    def split(self, input_fastq, barcode_fname, out_dir):
-        aligner = self.aligner
-        barcodes = FastaSet(FastaEntryIterator(barcode_fname))
-        score = self.score
-        missing = 0
-        out_fhndls = {}
-        for entry in FastqEntryIterator(input_fastq):
-            alignments = [(hdr, aligner.align(entry.seq, seq)) for hdr, seq in barcodes]
-            hdr, alignment = sorted(alignments, key=lambda x: x[1].score)
-            if alignment.score < score:
-                missing += 1
-                continue
-            if hdr not in out_fhndls:
-                out_fhndls[hdr] = open(os.path.join(out_dir, hdr), 'w')
-            fhndl = out_fhndls[hdr]
-            fhndl.write()
+from lhc.file_format.fasta import FastaEntryIterator
 
 
 def split(args):
-    splitter = FastqBarcodeSplitter(args.score, args.gap_penalty)
-    splitter.split(args.input, args.barcodes)
+    barcodes_ = [(hdr, seq.lower(), args.min_score if args.min_score else len(seq) - 1)
+                 for hdr, seq in FastaEntryIterator(args.barcodes)]
+    pool = multiprocessing.Pool(initializer=init_worker, initargs=[barcodes_, args.gap_penalty])
+    out_fhndls = {}
+    iterator = FastqEntryIterator(args.input)
+    for hdr, entry in pool.imap(find_barcode, iterator, 1000):
+        if hdr not in out_fhndls:
+            fname = '{}.fastq'.format(hdr)
+            out_fhndls[hdr] = open(os.path.join(args.output, fname), 'w')
+    for out_fhndl in out_fhndls.itervalues():
+        out_fhndl.close()
+
+aligner = None
+barcodes = None
+
+
+def init_worker(barcodes_, gap_penalty):
+    global aligner
+    global barcodes
+
+    aligner = LocalAligner(gap_penalty=gap_penalty)
+    barcodes = barcodes_
+
+
+def find_barcode(entry):
+    alignments = [(aligner.align(entry.seq.lower(), seq).score, hdr, min_score)
+                  for hdr, seq, min_score in barcodes]
+    score, hdr, min_score = sorted(alignments)[-1]
+    if score >= min_score:
+        hdr = 'None'
+    return hdr, entry
 
 
 def main():
-    args = get_parser().parse_args()
+    parser = get_parser()
+    args = parser.parse_args()
+    if args.output is None:
+        if args.input is sys.stdin:
+            parser.error('--output must be defined if --input not given.')
+        args.output = args.input.rsplit(',', 2)[0]
     args.func(args)
 
 
@@ -43,17 +58,19 @@ def get_parser():
 
 
 def define_parser(parser):
-    import sys
     add_arg = parser.add_argument
-    add_arg('input',
-            help='The input fastq file to split')
     add_arg('barcodes',
             help='A fasta file of sequences')
+    add_arg('-i', '--input', default=sys.stdin, action=OpenReadableFile,
+            help='The input fastq file to split (default: stdin)')
     add_arg('-O', '--output',
             help='The output directory.')
-    add_arg('-s', '--score', type=float,
+    add_arg('-s', '--min-score', type=float,
             help='The minimum score to allow a match (default: length of the barcode).')
-    add_arg('-p', '--penalty', type=int,
+    add_arg('-p', '--gap-penalty', type=int,
             help='The gap penalty (default: -10).')
     parser.set_defaults(func=split)
     return parser
+
+if __name__ == '__main__':
+    sys.exit(main())
