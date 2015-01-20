93a0f535e2ae8126c748ba1e96e291c2f68fcb54
Liam Childs <liam_childs@hotmail.com>
2015-01-20 14:18:50 +0100

    refactored gtf file format

--- lhc/file_format/gtf.py
+++ lhc/file_format/gtf.py
@@ -1,32 +1,11 @@
 import argparse
-import gzip
 
-from gtf_.index import IndexedGtfFile
-from gtf_.set_ import GtfSet
+from gtf_ import index
 from gtf_.iterator import GtfEntityIterator
 
 
 def iter_gtf(fname):
-    """ Convenience function """
     return GtfEntityIterator(fname)
-
-
-def index(fname):
-    """ Query a gtf file by model name. Use tabix for interval queries. """
-    index = []
-    fhndl = gzip.open(fname) if fname.endswith('.gz') else open(fname)
-    for line in fhndl:
-        if line.startswith('#'):
-            continue
-        type, ivl, attr = GtfEntityIterator._parse_line(line)
-        if type == 'gene':
-            index.append('{}\t{}\t{}\t{}'.format(attr['gene_name'], ivl.chr, ivl.start + 1, ivl.stop + 1))
-    fhndl.close()
-
-    iname = '{}.lhci'.format(fname)
-    fhndl = open(iname, 'w')
-    fhndl.write('\n'.join(index))
-    fhndl.close()
 
 
 def main():
@@ -39,8 +18,7 @@ def get_parser():
     subparsers = parser.add_subparsers()
 
     index_parser = subparsers.add_parser('index')
-    index_parser.add_argument('input')
-    index_parser.set_defaults(func=lambda args: index(args.input))
+    index.define_parser(index_parser)
 
     return parser
 
