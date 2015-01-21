cadefe07f4af609eaf5de5dbd2aeeceb5b5c8e9d
Liam Childs <liam_childs@hotmail.com>
2015-01-21 14:04:56 +0100

    added sorted list

--- lhc/collections/__init__.py
+++ lhc/collections/__init__.py
@@ -2,5 +2,6 @@ from augmented_tree import AugmentedTree
 from interval_tree import IntervalTree
 from nested_containment_list import NestedContainmentList
 from sorted_dict import SortedDict
+from sorted_list import SortedList
 from sorted_set import SortedSet
 from sorted_value_dict import SortedValueDict
