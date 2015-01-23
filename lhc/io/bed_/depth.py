3fc00155778e75a1e3eb148f962957ee5a8b3ba5
Liam Childs <liam_childs@hotmail.com>
2015-01-23 17:41:04 +0100

    changed depth calculation to be more memory intensive and less io intesive

--- lhc/io/bed_/depth.py
+++ lhc/io/bed_/depth.py
@@ -1,47 +1,49 @@
 import argparse
 import itertools
 import multiprocessing
+import sys
 
 from collections import Counter
-from pysam import AlignmentFile, TabixFile
+from pysam import AlignmentFile
 from lhc.argparse import OpenWritableFile
 from lhc.io.bed_.iterator import BedLineIterator
+from lhc.io.bed_.set_ import BedSet
 
 
 def depth(args):
-    interval_set = TabixFile(args.bed)
     alignment_file = AlignmentFile(args.bam)
     read_iterator, pool_iterator = itertools.tee(alignment_file)
     if args.cpus == 1:
-        init_worker(interval_set, alignment_file)
+        init_worker(args.bed, args.bam)
         it = itertools.imap(get_interval, pool_iterator)
     else:
-        initargs = [interval_set, alignment_file]
+        initargs = [args.bed, args.bam]
         pool = multiprocessing.Pool(args.cpus, initializer=init_worker, initargs=initargs)
-        it = pool.imap(get_interval, pool_iterator)
+        it = pool.imap(get_interval, pool_iterator, args.simultaneous_entries)
     cnt = Counter()
     for read, interval in itertools.izip(read_iterator, it):
         if interval is not None:
             cnt[interval.name] += 1
-    args.output.write(str(cnt))
+    for k, v in sorted(cnt.iteritems(), key=lambda x: x[1]):
+        args.output.write('{}\t{}\n'.format(k, v))
 
 interval_set = None
 alignment_file = None
 
 
-def init_worker(interval_set_, alignment_file_):
+def init_worker(bed_name, bam_name):
     global interval_set
     global alignment_file
-    interval_set = interval_set_
-    alignment_file = alignment_file_
+    interval_set = BedSet(BedLineIterator(bed_name))
+    alignment_file = AlignmentFile(bam_name)
 
 
 def get_interval(read):
     try:
-        intervals = [BedLineIterator.parse_line(line) for line in
-                     interval_set.fetch(alignment_file.getrname(read.reference_id),
-                                        read.reference_start, read.reference_end)]
-    except ValueError, e:
+        intervals = interval_set.get_overlapping_intervals(alignment_file.getrname(read.reference_id),
+                                                           read.reference_start, read.reference_end)
+    except KeyError, e:
+        intervals = []
         sys.stderr.write(str(e))
     if len(intervals) == 0:
         return None
@@ -68,6 +70,8 @@ def define_parser(parser):
     add_arg('-c', '--cpus', default=None, type=int)
     add_arg('-o', '--output', action=OpenWritableFile, default=sys.stdout,
             help='Where to put the output (default: stdout).')
+    add_arg('-s', '--simultaneous-entries', default=1000000, type=int,
+            help='The number of entries to submit to each worker.')
     parser.set_defaults(func=depth)
     return parser
 
