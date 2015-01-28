905ab6cdec4852835a192a591f2f8260acf8074a
Liam Childs <liam_childs@hotmail.com>
2015-01-28 10:04:56 +0100

    added bgzf to flexible opener

--- lhc/filetools/flexible_opener.py
+++ lhc/filetools/flexible_opener.py
@@ -1,13 +1,16 @@
 import bz2
 import gzip
 
+from Bio import bgzf
+
 
 def open_flexibly(fname):
-    if isinstance(fname, (file, bz2.BZ2File, gzip.GzipFile)):
+    if isinstance(fname, (file, bz2.BZ2File, gzip.GzipFile, bgzf.BgzfReader)):
         fhndl = fname
         fname = 'file'
     else:
         fhndl = bz2.BZ2File(fname) if fname.endswith('.bz2') else\
             gzip.open(fname) if fname.endswith('.gz') else\
+            bgzf.open(fname) if fname.endswith('.bgz') else\
             open(fname)
     return fname, fhndl
