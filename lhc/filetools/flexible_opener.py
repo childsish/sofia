d42ee0655ce8d2d094fb16fa7219631c762fe4c1
Liam Childs <liam_childs@hotmail.com>
2015-01-28 14:54:00 +0100

    added string io to flexible opener

--- lhc/filetools/flexible_opener.py
+++ lhc/filetools/flexible_opener.py
@@ -1,16 +1,18 @@
-import bz2
 import gzip
 
 from Bio import bgzf
+from StringIO import StringIO
 
 
 def open_flexibly(fname):
-    if isinstance(fname, (file, bz2.BZ2File, gzip.GzipFile, bgzf.BgzfReader)):
+    if isinstance(fname, (file, gzip.GzipFile, bgzf.BgzfReader)):
         fhndl = fname
         fname = 'file'
+    elif isinstance(fname, StringIO):
+        fhndl = fname
+        fname = 'stringio'
     else:
-        fhndl = bz2.BZ2File(fname) if fname.endswith('.bz2') else\
-            gzip.open(fname) if fname.endswith('.gz') else\
+        fhndl = gzip.open(fname) if fname.endswith('.gz') else\
             bgzf.open(fname) if fname.endswith('.bgz') else\
             open(fname)
     return fname, fhndl
