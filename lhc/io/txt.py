0cc465cccf36afce7ab68a99a63d9c647221aaad
Liam Childs <liam_childs@hotmail.com>
2015-02-09 14:33:57 +0100

    added text file compression and indexing

--- lhc/io/txt.py
+++ lhc/io/txt.py
@@ -1,6 +1,11 @@
 import argparse
 
-from txt_ import sorter
+from txt_ import sorter, compress
+
+
+def main():
+    args = get_parser().parse_args()
+    args.func(args)
 
 
 def get_parser():
@@ -10,7 +15,14 @@ def get_parser():
 def define_parser(parser):
     subparsers = parser.add_subparsers()
 
-    sort_parser = subparsers.add_parser()
+    sort_parser = subparsers.add_parser('sort')
     sorter.define_parser(sort_parser)
 
+    compress_parser = subparsers.add_parser('compress')
+    compress.define_parser(compress_parser)
+
     return parser
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main())
---
+++ lhc/io/txt_/compress.py
@@ -0,0 +1,86 @@
+import argparse
+import json
+
+from tracker_factory import TrackerFactory
+from index import FileIndex
+from Bio.bgzf import BgzfWriter
+
+
+class Partitioner(object):
+    def __init__(self, fname, column_trackers):
+        self.buffer = ''
+        self.fhndl = open(fname)
+        self.column_trackers = column_trackers
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.buffer is None:
+            raise StopIteration
+        for line in self.fhndl:
+            parts = line.rstrip('\r\n').split('\t')
+            is_new_entry = [tracker.is_new_entry(parts) for tracker in self.column_trackers]
+            if any(is_new_entry):
+                key = tuple(tracker.get_key() for tracker in self.column_trackers)
+                buffer = self.buffer
+
+                self.buffer = line
+                for tracker in self.column_trackers:
+                    tracker.set_new_entry(parts)
+                return key, buffer, any(is_new_entry[:-1])
+            self.buffer += line
+        buffer = self.buffer
+        self.buffer = None
+        return tuple(tracker.get_key() for tracker in self.column_trackers), buffer, False
+
+
+def compress(args):
+    if not args.extension.startswith('.'):
+        args.extension = '.' + args.extension
+
+    factory = TrackerFactory()
+    column_trackers = [factory.make(column_type) for column_type in args.column_types]
+    partitioner = Partitioner(args.input, column_trackers)
+    index = FileIndex(len(args.column_types))
+    out_fhndl = BgzfWriter(args.input + args.extension)
+    key, buffer, break_block = partitioner.next()
+    index[key] = out_fhndl.tell()
+    for key, data, break_block in partitioner:
+        if len(buffer) + len(data) > 65536 or break_block:
+            out_fhndl.write(buffer)
+            out_fhndl.flush()
+            index[key] = out_fhndl.tell()
+            buffer = ''
+        buffer += data
+    out_fhndl.close()
+
+    out_fhndl = open(args.input + args.extension + '.lci', 'w')
+    json.dump(index.__getstate__(), out_fhndl, indent=4, separators=(',', ': '))
+    out_fhndl.close()
+
+
+def main():
+    args = get_parser().parse_args()
+    args.func(args)
+
+
+def get_parser():
+    return define_parser(argparse.ArgumentParser())
+
+
+def define_parser(parser):
+    add_arg = parser.add_argument
+    add_arg('input',
+            help='input file name')
+    add_arg('-c', '--column-types', nargs='+',
+            help='types for converting columns ')
+    add_arg('-e', '--extension', default='.bgz',
+            help='compressed file extension')
+    parser.set_defaults(func=compress)
+    return parser
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main())
