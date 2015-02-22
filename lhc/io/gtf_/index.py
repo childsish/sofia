be6753c67af74cea1497af1e92e9599585270e8d
Liam Childs <liam_childs@hotmail.com>
2015-02-22 21:29:07 +0100

    fixed incomplete gene bzg in gtf and gff

--- lhc/io/gtf_/index.py
+++ lhc/io/gtf_/index.py
@@ -1,4 +1,4 @@
-from iterator import GtfEntryIterator
+from iterator import GtfEntryIterator, GtfLineIterator
 
 
 class IndexedGtfFile(object):
@@ -6,4 +6,12 @@ class IndexedGtfFile(object):
         self.index = index
 
     def fetch(self, chr, start, stop):
-        return GtfEntryIterator.get_features(self.index.fetch(chr, start, stop))
+        lines = [GtfLineIterator.parse_line(line) for line in self.index.fetch(chr, start, stop)]
+        return [self.get_features(line) for line in lines if line.type == 'gene']
+
+    def get_features(self, gene_line):
+        genes = GtfEntryIterator.get_features(self.index.fetch(gene_line.chr, gene_line.start, gene_line.stop))
+        for gene in genes:
+            if gene.name == gene_line.attr['gene_id']:
+                return gene
+        raise KeyError('{}'.format(gene_line.attr['gene_id']))
