03d1f71d789af984782fc74f05756b29f8cb8305
Liam Childs <liam_childs@hotmail.com>
2015-04-02 20:57:55 +0200

    reimplemented genomic_feature get_abs_pos

--- lhc/binf/genomic_feature.py
+++ lhc/binf/genomic_feature.py
@@ -61,18 +61,17 @@ class GenomicFeature(Interval):
     # Position functions
     
     def get_abs_pos(self, pos, partial_rel_pos=0):
-        raise NotImplementedError()
-        # if pos < 0:
-        #     raise IndexError('relative position {} not contained within {}'.format(pos, self))
-        # length = 0
-        # for child in self.children:
-        #     child_length = len(child)
-        #     if partial_rel_pos + length + child_length > pos:
-        #         return child.get_abs_pos(pos, partial_rel_pos + length)
-        #     length += child_length
-        # if length > 0:
-        #     raise IndexError('relative position {} not contained within {}'.format(pos, self))
-        # return self.start + pos - partial_rel_pos
+        if len(self.children) == 0:
+            return self.start + pos if self.strand == '+' else self.stop + pos - len(self)
+
+        children = self.children if self.strand == '+' else reversed(self.children)
+        fr = 0
+        for child in children:
+            length = len(child)
+            if fr <= pos < fr + length:
+                return child.get_abs_pos(pos - fr)
+            fr += length
+        raise IndexError('relative position {} not contained within {}'.format(pos, self))
     
     def get_rel_pos(self, pos, types=None):
         if len(self.children) == 0:
