c6787c4bd48c6b9c6b6bfde9ff97aebe36518292
Liam Childs <liam_childs@hotmail.com>
2015-04-02 18:00:29 +0200

    added secondary structure feature calculation

--- templates/genomics/steps/rna_structure.py
+++ templates/genomics/steps/rna_structure.py
@@ -1,19 +1,6 @@
 from sofia_.step import Step
 
 
-class DummyRNAModule(object):
-    def fold(self, seq):
-        return None
-
-    def pf_fold(self, seq):
-        return None
-
-try:
-    import RNA
-except ImportError:
-    RNA = DummyRNAModule()
-
-
 class GetTranslationStartMinimumFreeEnergy(Step):
     """
     Get the minimum free energy around the start codon of the major transcript.
@@ -29,6 +16,7 @@ class GetTranslationStartMinimumFreeEnergy(Step):
         :param type: (mfe, efe)
         :return:
         """
+        import RNA
         self.offset = offset
         self.fold = RNA.fold if type == 'mfe' else RNA.pf_fold
 
@@ -42,58 +30,57 @@ class GetTranslationStartMinimumFreeEnergy(Step):
 
 class GetStructuralFeatures(Step):
     """
-    Incomplete. Get the RNA secondary structural features of the given sequence.
+    Get the RNA secondary structural features of the given secondary structure.
     """
 
     IN = ['rna_secondary_structure']
-    OUT = ['rna_secondary_structure_features']
+    OUT = ['hairpin_loop', 'multiloop', 'internal_loop', 'bulge', 'stem', 'branch', 'bridge']
 
+    def calculate(self, rna_secondary_structure):
+        hloops = []
+        mloops = []
+        iloops = []
+        bulges = []
+        stems = []
+        branches = []
+        bridges = []
 
-def structural_features(stc):
-    hloops = []
-    mloops = []
-    iloops = []
-    bulges = []
-    stems = []
-    branches = []
-    bridges = []
-    
-    lvls = [[]]
-    dots = []
-    c_lvl = 0
-    c_stem = 0
-    for i in xrange(len(stc)):
-        if stc[i] == '(':
-            if c_lvl == 0:
-                bridges.append(len(dots))
-                dots = []
-            lvls[c_lvl].append('(')
-            lvls.append([])
-            c_lvl += 1
-        elif stc[i] == ')':
-            p_lvl = lvls.pop()
-            c_lvl -= 1
-            
-            if p_lvl.count('(') == 0:
-                hloops.append(len(p_lvl))
-            elif p_lvl.count('(') == 1 and p_lvl.count('.') > 0:
-                if p_lvl[0] == '.' and p_lvl[-1] == '.':
-                    iloops.append((p_lvl.index('('), len(p_lvl) - p_lvl.index('(') - 1))
-                else:
-                    bulges.append(len(p_lvl)-1)
-            elif '.' in p_lvl:
-                mloops.append(len(p_lvl) - p_lvl.count('('))
-                branches.append(p_lvl.count('('))
-            
-            if p_lvl != ['('] and c_stem != 0:
-                stems.append(c_stem)
-                c_stem = 0
-            c_stem += 1
-        elif stc[i] == '.':
-            if c_lvl == 0:
-                dots.append(i)
-            lvls[c_lvl].append('.')
-    bridges.append(len(dots))
-    stems.append(c_stem)
+        lvls = [[]]
+        dots = []
+        c_lvl = 0
+        c_stem = 0
+        for i in xrange(len(rna_secondary_structure)):
+            if rna_secondary_structure[i] == '(':
+                if c_lvl == 0:
+                    bridges.append(len(dots))
+                    dots = []
+                lvls[c_lvl].append('(')
+                lvls.append([])
+                c_lvl += 1
+            elif rna_secondary_structure[i] == ')':
+                p_lvl = lvls.pop()
+                c_lvl -= 1
 
-    return hloops, mloops, iloops, bulges, stems, branches, bridges
+                if p_lvl.count('(') == 0:
+                    hloops.append(len(p_lvl))
+                elif p_lvl.count('(') == 1 and p_lvl.count('.') > 0:
+                    if p_lvl[0] == '.' and p_lvl[-1] == '.':
+                        iloops.append((p_lvl.index('('), len(p_lvl) - p_lvl.index('(') - 1))
+                    else:
+                        bulges.append(len(p_lvl)-1)
+                elif '.' in p_lvl:
+                    mloops.append(len(p_lvl) - p_lvl.count('('))
+                    branches.append(p_lvl.count('('))
+
+                if p_lvl != ['('] and c_stem != 0:
+                    stems.append(c_stem)
+                    c_stem = 0
+                c_stem += 1
+            elif rna_secondary_structure[i] == '.':
+                if c_lvl == 0:
+                    dots.append(i)
+                lvls[c_lvl].append('.')
+        bridges.append(len(dots))
+        stems.append(c_stem)
+
+        return hloops, mloops, iloops, bulges, stems, branches, bridges
