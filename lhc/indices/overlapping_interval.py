87b494dbd0a7a70337acea2ccf84be7b8257e836
Liam Childs <liam_childs@hotmail.com>
2015-05-07 14:38:52 +0200

    fixed overlapping interval bug

--- lhc/indices/overlapping_interval.py
+++ lhc/indices/overlapping_interval.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from lhc.indices.index import Accessor
-from lhc.interval import Interval, IntervalBinner
+from lhc.interval import IntervalBinner
+
 
 class OverlappingIntervalIndex(Accessor):
     
@@ -11,30 +12,23 @@ class OverlappingIntervalIndex(Accessor):
     
     def __init__(self):
         self.binner = IntervalBinner()
-        self.bins = defaultdict(dict)
+        self.bins = defaultdict(list)
     
     def __contains__(self, key):
         bin = self.binner.get_bin(key)
-        return bin in self.bins and key in self.bins[bin]
+        return any(key == k for k, v in self.bins.get(bin, []))
     
     def __getitem__(self, key):
         res = []
         bins = self.binner.get_overlapping_bins(key)
         for bin_fr, bin_to in bins:
-            if bin_fr == bin_to:
-                if bin_fr not in self.bins:
-                    continue
-                res.extend(v for k, v in self.bins[bin_fr].iteritems() if k.overlaps(key))
-            else:
-                for bin in xrange(bin_fr, bin_to):
-                    if bin not in self.bins:
-                        continue
-                    res.extend(v for k, v in self.bins[bin_fr].iteritems() if k.overlaps(key))
+            for bin in xrange(bin_fr, bin_to + (bin_fr == bin_to)):
+                res.extend(v for k, v in self.bins.get(bin, []) if k.overlaps(key))
         return res
     
     def __setitem__(self, key, value):
         bin = self.binner.get_bin(key)
-        self.bins[bin][Interval(key.start, key.stop)] = value
+        self.bins[bin].append((key, value))
     
     def __getstate__(self):
         return dict((attr, getattr(self, attr)) for attr in self.__slots__)
