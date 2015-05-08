0baddb887691cff84585621c7418a0e09098798b
Liam Childs <liam_childs@hotmail.com>
2015-05-08 15:10:51 +0200

    updated indexing

--- lhc-python/lhc/indices/point_index.py
+++ lhc-python/lhc/indices/point_index.py
@@ -1,4 +1,4 @@
-from bisect import bisect_left, bisect_right
+from bisect import bisect
 from lhc.indices.index import Index
 
 
@@ -12,16 +12,17 @@ class PointIndex(Index):
         self.values = []
     
     def __contains__(self, key):
-        idx = bisect_right(self.keys, key) - 1
-        return self.keys[idx] == key
+        return self.keys[bisect(self.keys, key) - 1] == key
     
     def __getitem__(self, key):
-        idx = bisect_right(self.keys, key) - 1
-        return None if idx == -1 else self.values[idx]
+        idx = bisect(self.keys, key) - 1
+        if idx < 0:
+            raise KeyError(key)
+        return self.values[idx]
     
     def __setitem__(self, key, value):
-        idx = bisect_left(self.keys, key)
-        if idx == len(self.keys) or self.keys[idx] != key:
+        idx = bisect(self.keys, key)
+        if idx == len(self.keys) or self.keys[idx - 1] != key:
             self.keys.insert(idx, key)
             self.values.insert(idx, value)
         else:
