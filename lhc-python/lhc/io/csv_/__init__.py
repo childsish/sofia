6b071dd46264966092db768e46b1be3ce6117cca
Liam Childs <liam_childs@hotmail.com>
2015-05-10 17:33:18 +0200

    added csv index parser

--- lhc-python/lhc/io/csv_/__init__.py
+++ lhc-python/lhc/io/csv_/__init__.py
@@ -1,5 +1,6 @@
 from entry_guesser import EntryGuesser
 from entity import Entity, Column
 from entity_parser import EntityParser
+from index_parser import IndexParser
 from iterator import CsvIterator
 from set_ import CsvSet
---
+++ lhc-python/lhc/io/csv_/index_parser.py
@@ -0,0 +1,22 @@
+import re
+
+from lhc.indices import KeyIndex, IntervalIndex
+
+
+class IndexParser(object):
+
+    REGX = re.compile('(?P<code>\w+)(?P<column>\d+)')
+    INDICES = {
+        'k': KeyIndex,  # (k)ey index
+        'i': IntervalIndex,  # (i)nterval index
+    }
+
+    def parse_definition(self, definition):
+        match = self.REGX.match(definition)
+        if not match:
+            raise ValueError('invalid index definition')
+        return self.INDICES[match.group('code')], int(match.group('column'))
+
+    @classmethod
+    def register_index(cls, code, index):
+        cls.INDICES[code] = index
