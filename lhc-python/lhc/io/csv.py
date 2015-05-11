7068e2b110936b8a580cf22613076da604df98dc
Liam Childs <liam_childs@hotmail.com>
2015-05-11 15:53:42 +0200

    moved txt sort to csv sort

--- lhc-python/lhc/io/csv.py
+++ lhc-python/lhc/io/csv.py
@@ -1,6 +1,30 @@
-import os
+import argparse
+
+from csv_.iterator import CsvIterator
+from csv_ import sort
 
 
-def iter_csv(fname, skip=0):
-    it = iter_dir if os.path.isdir(fname) else iter_file
-    return it(fname, column_builder, field_builder, skip)
+def iter_csv(fname, entry=None, comment='#', skip=0, delimiter='\t'):
+    yield CsvIterator(fname, entry, comment, skip, delimiter)
+
+
+def main():
+    args = get_parser().parse_args()
+    args.func(args)
+
+
+def get_parser():
+    return define_parser(argparse.ArgumentParser())
+
+
+def define_parser(parser):
+    subparsers = parser.add_subparsers()
+
+    sort_parser = subparsers.add_parser('sort')
+    sort.define_parser(sort_parser)
+
+    return parser
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main())
