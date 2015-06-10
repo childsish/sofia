e21706984bdaf09758ebe76656d2d77815dce6f0
Liam Childs <liam_childs@hotmail.com>
2015-06-10 17:33:53 +0200

    made various adjustments to intervals and interval containers

--- lhc-python/lhc/collections/__init__.py
+++ lhc-python/lhc/collections/__init__.py
@@ -4,3 +4,5 @@ from sorted_dict import SortedDict
 from sorted_list import SortedList
 from sorted_set import SortedSet
 from sorted_value_dict import SortedValueDict
+from nested_containment_list import NestedContainmentList
+from interval_binner import IntervalBinner
