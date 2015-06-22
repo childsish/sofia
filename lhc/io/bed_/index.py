d2028625bcb6b06fd6fd6d5c0a375f71c0de49ae
Liam Childs <liam_childs@hotmail.com>
2015-06-22 14:44:15 +0200

    minor updates

--- lhc/io/bed_/index.py
+++ lhc/io/bed_/index.py
@@ -5,5 +5,7 @@ class IndexedBedFile(object):
     def __init__(self, index):
         self.index = index
 
-    def fetch(self, chr, start, stop):
+    def fetch(self, chr, start, stop=None):
+        if stop is None:
+            stop = start + 1
         return [BedLineIterator.parse_line(line) for line in self.index.fetch(chr, start, stop)]
