a1f6915014c55c9117e5161ce547195d8b22ff09
Liam Childs <liam_childs@hotmail.com>
2015-06-22 12:52:34 +0200

    changed entity parser delimiters to be win friendly

--- lhc/io/txt_/entity_parser.py
+++ lhc/io/txt_/entity_parser.py
@@ -8,24 +8,35 @@ from lhc.interval import Interval
 class EntityParser(object):
     """
     Grammar
-    <entity> ::= (<name>.)?<type><definition>
-    <definition> ::= <column> | "[" <entity> ("," <entity>)* "]"
+    -------
+    <entity> ::= ( <name> "-" )? <type> <definition>
+    <definition> ::= <column> | "[" <entity> ("." <entity>)* "]"
     <name> ::= [a-zA-Z_]+
     <type> ::= [a-zA-Z]+
     <column> ::= [0-9]+  # 1-indexed
 
     Examples
+    --------
     s1       - create a string using column 1.
-    r[i2,i3] - create integers from columns 2 and 3 then create an interval from the integers
+    r[i2.i3] - create integers from columns 2 and 3 then create an interval from the integers
+    chr-s1.pos-i4
+
+    Notes
+    -----
+    * Can not use ',' in windows command lines.
     """
 
-    NAME_AND_TYPE = frozenset(string.ascii_letters + '_.')
+    NAME_AND_TYPE = frozenset(string.ascii_letters + '_-')
     TYPES = {
         's': str,  # (s)tring
         'i': int,  # (i)nteger
         'f': float,  # (f)loat
         'r': Interval  # (r)ange
     }
+    NAME_DELIMITER = '-'
+    FIELD_DELIMITER = '.'
+    OPEN_SUBENTITY = '['
+    CLOSE_SUBENTITY = ']'
 
     def __init__(self):
         self.pos = None
@@ -34,7 +45,8 @@ class EntityParser(object):
         self.pos = 0
         res = self._parse_definition(definition)
         res = res[0] if len(res) == 1 else\
-            Entity(namedtuple('Entry', [('V{}'.format(i + 1) if r.name is None else r.name) for i, r in enumerate(res)]),
+            Entity(namedtuple('Entry', [('V{}'.format(i + 1) if r.name is None else r.name)
+                                        for i, r in enumerate(res)]),
                    res, 'Entry')
         return res
 
@@ -44,9 +56,9 @@ class EntityParser(object):
             fr = self.pos
             while self.pos < len(definition) and definition[self.pos] in self.NAME_AND_TYPE:
                 self.pos += 1
-            name, type = ([None] + definition[fr:self.pos].split('.', 1))[-2:]
+            name, type = ([None] + definition[fr:self.pos].split(self.NAME_DELIMITER, 1))[-2:]
             type = self.TYPES[type]
-            if definition[self.pos] == '[':
+            if definition[self.pos] == self.OPEN_SUBENTITY:
                 self.pos += 1
                 entities = self._parse_definition(definition)
                 res.append(Entity(type, entities, name))
@@ -65,9 +77,9 @@ class EntityParser(object):
                 raise ValueError('invalid entity definition: {}'.format(definition))
 
             if self.pos < len(definition):
-                if definition[self.pos] == ',':
+                if definition[self.pos] == self.FIELD_DELIMITER:
                     self.pos += 1
-                elif definition[self.pos] == ']':
+                elif definition[self.pos] == self.CLOSE_SUBENTITY:
                     break
         return res
 
--- lhc/test/test_io/test_txt/test_entity_parser.py
+++ lhc/test/test_io/test_txt/test_entity_parser.py
@@ -13,7 +13,7 @@ class TestEntityParser(unittest.TestCase):
 
         self.assertEquals(4, parser.parse_definition('i3')(row))
         self.assertEquals('test', parser.parse_definition('s4')(row))
-        self.assertEquals(Interval(2, 3), parser.parse_definition('r[i1,i2]')(row))
+        self.assertEquals(Interval(2, 3), parser.parse_definition('r[i1.i2]')(row))
 
     def test_register_entity(self):
         parser = EntityParser()
@@ -21,12 +21,12 @@ class TestEntityParser(unittest.TestCase):
 
         row = ['2', '3', '4', 'test']
 
-        self.assertEquals(GenomicInterval('test', 2, 3), parser.parse_definition('gr[s4,i1,i2]')(row))
+        self.assertEquals(GenomicInterval('test', 2, 3), parser.parse_definition('gr[s4.i1.i2]')(row))
 
     def test_multiple_output(self):
         parser = EntityParser()
 
-        entity_factory = parser.parse_definition('i1,test.i2,i3,s4')
+        entity_factory = parser.parse_definition('i1.test-i2.i3.s4')
         res = entity_factory(['2', '3', '4', 'test'])
 
         self.assertEquals(2, res[0])
@@ -36,7 +36,7 @@ class TestEntityParser(unittest.TestCase):
     def test_multiple_nested_output(self):
         parser = EntityParser()
 
-        entity_factory = parser.parse_definition('r[i2,i3],f5')
+        entity_factory = parser.parse_definition('r[i2.i3].f5')
 
         self.assertIsInstance(entity_factory.entities[0], Entity)
         self.assertIsInstance(entity_factory.entities[1], Column)
