1e1a8600515ff16dd266dcb6f6ab42abaaecb514
Liam Childs <liam_childs@hotmail.com>
2015-06-29 12:04:16 +0200

    added index parser

--- lhc-python/lhc/io/txt_/index_parser.py
+++ lhc-python/lhc/io/txt_/index_parser.py
@@ -1,22 +1,27 @@
-import re
-
-from lhc.indices import KeyIndex, IntervalIndex
+from lhc.interval import Interval
+from lhc.indices.bgzf import PointIndex, IntervalIndex
 
 
 class IndexParser(object):
 
-    REGX = re.compile('(?P<code>\w+)(?P<column>\d+)')
-    INDICES = {
-        'k': KeyIndex,  # (k)ey index
-        'i': IntervalIndex,  # (i)nterval index
+    TYPES = {
+        str: PointIndex,  # (s)tring
+        int: PointIndex,  # (i)nteger
+        float: PointIndex,  # (f)loat
+        Interval: IntervalIndex  # (r)ange
     }
 
-    def parse_definition(self, definition):
-        match = self.REGX.match(definition)
-        if not match:
-            raise ValueError('invalid index definition: {}'.format(definition))
-        return self.INDICES[match.group('code')], int(match.group('column'))
+    def __init__(self):
+        self.pos = None
+
+    def parse(self, definition):
+        if definition.name == 'Entry':
+            indices = tuple(self.TYPES[d.type] for d in definition.entities)
+            return indices[0](indices[1:])
+        return self.TYPES[definition.type]()
 
     @classmethod
-    def register_index(cls, code, index):
-        cls.INDICES[code] = index
+    def register_index(cls, type, index):
+        if type in cls.TYPES:
+            raise KeyError('type "{}" already taken'.format(type))
+        cls.TYPES[type] = index
