746fa9503b44f9107d4a9a3ca7663bdc0614ba01
Liam Childs <liam_childs@hotmail.com>
2015-07-21 12:43:06 +0200

    fixed resolved entity names

--- sofia_/entity.py
+++ sofia_/entity.py
@@ -4,4 +4,6 @@ class Entity(object):
         self.attr = {} if attr is None else attr
 
     def __str__(self):
-        return self.name + ':' + ':'.join('{}={}'.format(k, ','.join(v)) for k, v in self.attr.iteritems())
+        res = [self.name]
+        res.extend('{}={}'.format(k, ','.join(v)) for k, v in self.attr.iteritems())
+        return ':'.join(res)
