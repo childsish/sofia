5a4fdda7bea95dcfcaead0a098fe2af054dec6ff
Liam Childs <liam_childs@hotmail.com>
2015-07-21 18:41:33 +0200

    made gbk iterator produce own object

--- templates/genomics/steps/gbk.py
+++ templates/genomics/steps/gbk.py
@@ -10,11 +10,17 @@ class GbkIterator(Target):
 
     EXT = ['.gbk', '.gbk.gz']
     FORMAT = 'gbk'
-    OUT = ['genomic_feature']
+    OUT = ['genomic_interval']
 
     def get_interface(self, filename):
         fileobj = gzip.open(filename) if filename.endswith('.gz') else open(filename)
         return iter(Iterator(fileobj))
+
+    def calculate(self):
+        res = self.interface.next()
+        while res.type != 'CDS':
+            res = self.interface.next()
+        return res
 
 
 class GbkSet(Resource):
--- templates/genomics/steps/genomic_feature.py
+++ templates/genomics/steps/genomic_feature.py
@@ -18,3 +18,15 @@ class GetMajorTranscriptFromGenomicFeature(Step):
     @staticmethod
     def get_transcript_length(transcript):
         return sum(len(child) for child in transcript.children if child.type == 'CDS')
+
+
+class GetCodingSequenceFromGenomicInterval(Step):
+    """
+    Get the major transcript of a genomic feature. Defined as the longest transcript (ie. most complete).
+    """
+
+    IN = ['genomic_interval', 'chromosome_sequence_set']
+    OUT = ['coding_sequence']
+
+    def calculate(self, genomic_interval, chromosome_sequence_set):
+        return genomic_interval.get_sub_seq(chromosome_sequence_set)
