684efe1128db3fc7bb4f6785ee7130eb7932bfe7
Liam Childs <liam_childs@hotmail.com>
2015-08-11 16:17:55 +0200

    updated chromosome sequence step

--- templates/genomics/steps/chromosome_sequence.py
+++ templates/genomics/steps/chromosome_sequence.py
@@ -10,17 +10,15 @@ class GetHomopolymer(Step):
     OUT = ['homopolymer']
 
     def calculate(self, variant, chromosome_sequence_set):
-        res = []
-        for alt in variant.alt.split(','):
-            ref = variant.ref
-            i = 0
-            while i < len(ref) and i < len(alt) and ref[i] == alt[i]:
-                i += 1
-            pos = variant.pos + i
-            seq = chromosome_sequence_set.fetch(variant.chr, pos, pos + 100)
-            
-            i = 0
-            while seq[i] == seq[0]:
-                i += 1
-            res.append(seq[:i])
-        return res
+        ref = variant.ref
+        alt = variant.alt
+        i = 0
+        while i < len(ref) and i < len(alt) and ref[i] == alt[i]:
+            i += 1
+        pos = variant.pos + i
+        seq = chromosome_sequence_set.fetch(variant.chr, pos, pos + 100)
+
+        i = 0
+        while seq[i] == seq[0]:
+            i += 1
+        return seq[:i]
---
+++ templates/genomics/test/test_chromosome_sequence.py
@@ -0,0 +1,25 @@
+__author__ = 'Liam'
+
+import unittest
+
+from templates.genomics.steps.chromosome_sequence import GetHomopolymer
+from lhc.io.vcf_.iterator import Variant
+from lhc.io.fasta_.set_ import FastaSet
+
+
+class TestChromosomeSequence(unittest.TestCase):
+    def test_get_homopolymer(self):
+        step = GetHomopolymer()
+
+        chromosome_sequence_set = FastaSet([('1', 20 * 'a' + 80 * 'b')])
+        v1 = Variant('1', 0, None, 'b', 'a')
+        v2 = Variant('1', 0, None, 'b', 'ba')
+        v3 = Variant('1', 0, None, 'ba', 'b')
+
+        self.assertEquals(20 * 'a', step.calculate(v1, chromosome_sequence_set))
+        self.assertEquals(19 * 'a', step.calculate(v2, chromosome_sequence_set))
+        self.assertEquals(19 * 'a', step.calculate(v3, chromosome_sequence_set))
+
+
+if __name__ == '__main__':
+    unittest.main()
