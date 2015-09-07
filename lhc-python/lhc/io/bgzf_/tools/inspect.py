bd1ec59785a856bbbd90270857f7e091f2ba36e8
Liam Childs <liam_childs@hotmail.com>
2015-09-07 15:09:08 +0200

    added simple bgzf block inspection

---
+++ lhc-python/lhc/io/bgzf_/tools/inspect.py
@@ -0,0 +1,42 @@
+__author__ = 'Liam Childs'
+
+import argparse
+
+from Bio.bgzf import _load_bgzf_block
+
+
+def inspect(handle):
+    data_start = 0
+    i = 0
+    while True:
+        block_length, data = _load_bgzf_block(handle)
+        data_len = len(data)
+        print 'block {}'.format(i)
+        print repr(data[:data.find('\n') + 1])
+        print repr(data[data.rfind('\n', 0, len(data) - 1) + 1:])
+        data_start += data_len
+        i += 1
+
+
+def main():
+    args = get_parser().parse_args()
+    args.func(args)
+
+
+def get_parser():
+    return define_parser(argparse.ArgumentParser())
+
+
+def define_parser(parser):
+    add_arg = parser.add_argument
+    add_arg('input', nargs='?',
+            help='input file name (default: stdin)')
+    parser.set_defaults(func=init_inspect)
+    return parser
+
+
+def init_inspect(args):
+    import sys
+    input = sys.stdin if args.input is None else open(args.input, 'rb')
+    inspect(input)
+    input.close()
