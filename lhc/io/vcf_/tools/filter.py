16193fc1bfa4b1e3e72d87e82e7ffb2e34673929
Liam Childs <liam_childs@hotmail.com>
2015-09-11 16:10:26 +0200

    unified txt/vcf filtering

--- lhc/io/vcf_/tools/filter.py
+++ lhc/io/vcf_/tools/filter.py
@@ -1,21 +1,17 @@
 import argparse
 
 from ..iterator import VcfLineIterator
-from lhc.argparse import OpenReadableFile, OpenWritableFile
+from lhc.io.txt_ import Filter
 
 
-def filter(args):
-    it = VcfLineIterator(args.input)
-    fhndl = sys.stdout if args.output is None else open(args.output, 'w')
+def filter(input, output, filters):
+    it = VcfLineIterator(input)
+    filtered_it = Filter(it, filters, {'NOCALL': 'NOCALL', 'PASS': 'PASS'})
     for k, vs in it.hdrs.iteritems():
-        for v in vs:
-            fhndl.write('{}={}\n'.format(k, v))
-    fhndl.write('#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t' + '\t'.join(it.samples) + '\n')
-    for line in it:
-        if float(line.qual) < args.quality:
-            continue
-        fhndl.write(line)
-    fhndl.close()
+        output.write('\n'.join('{}={}'.format(k, v) for v in vs))
+    output.write('#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t' + '\t'.join(it.samples) + '\n')
+    for line in filtered_it:
+        output.write(line)
 
 
 def main():
@@ -28,16 +24,24 @@ def get_parser():
 
 
 def define_parser(parser):
-    import sys
     add_arg = parser.add_argument
-    add_arg('-i', '--input', default=sys.stdin, action=OpenReadableFile,
-            help='The name of the vcf_ file to be filtered.')
-    add_arg('-o', '--output', default=sys.stdout, action=OpenWritableFile,
-            help='The name of the filtered vcf_ file.')
-    add_arg('-q', '--quality', type=float, default=0,
-            help='Variants below the given quality will be filtered (default: 0).')
-    parser.set_defaults(func=filter)
+    add_arg('input', nargs='?',
+            help='The name of the vcf_ file to be filtered (default: stdin).')
+    add_arg('output', nargs='?',
+            help='The name of the filtered vcf_ file (default: stdout).')
+    add_arg('-f', '--filter', nargs='+', default=[],
+            help='Filters to apply (default: none).')
+    parser.set_defaults(func=filter_init)
     return parser
+
+
+def filter_init(args):
+    import sys
+    input = sys.stdin if args.input is None else open(args.input)
+    output = sys.stdout if args.output is None else open(args.output, 'w')
+    filter(input, output, args.filters)
+    input.close()
+    output.close()
 
 
 if __name__ == '__main__':
