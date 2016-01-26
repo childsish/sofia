17059aa78882c875c042a2c7bf98938157fd0e19
Liam Childs <liam_childs@hotmail.com>
2016-01-26 12:56:49 +0100

    added attribute map caching

--- sofia_/attribute_map_factory.py
+++ sofia_/attribute_map_factory.py
@@ -6,11 +6,20 @@ class AttributeMapFactory(object):
         self.hdrs = fhndl.readline().strip().split('\t')
         fhndl.close()
 
+        self.cache = {}
+
     def make(self, fr, to):
+        if (fr, to) in self.cache:
+            return self.cache[(fr, to)]
+
         fr_idx = self.hdrs.index(fr)
         to_idx = self.hdrs.index(to)
         fhndl = open(self.fname)
         fhndl.next()
         attribute_map = {parts[fr_idx].strip(): parts[to_idx].strip() for parts in (line.split('\t') for line in fhndl)}
         fhndl.close()
+
+        if (fr, to) not in self.cache:
+            self.cache[(fr, to)] = attribute_map
+
         return attribute_map
