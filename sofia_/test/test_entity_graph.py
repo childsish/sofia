443a80f511645e9f21b01a8865f4993b6c601d3f
Liam Childs <liam_childs@hotmail.com>
2016-02-10 22:34:48 +0100

    updated unit tests

--- sofia_/test/test_entity_graph.py
+++ sofia_/test/test_entity_graph.py
@@ -12,7 +12,8 @@ class TestEntityGraph(unittest.TestCase):
     def test_get_descendents_paths(self):
         paths = sorted(self.graph.get_descendent_paths('variant'))
         paths = {path[-1]['name']: path for path in paths}
-        self.assertEquals(len(paths), 4)
+        self.assertEquals(len(paths), 5)
+        self.assertEquals([{'type': 'attr', 'name': 'chromosome_id', 'key': 'chr'}], paths['chromosome_id'])
         self.assertEquals([{'type': 'attr', 'name': 'chromosome_pos', 'key': 'pos'}], paths['chromosome_pos'])
         self.assertEquals([{'type': 'attr', 'name': 'reference_allele', 'key': 'ref'}], paths['reference_allele'])
         self.assertEquals([{'type': 'attr', 'name': 'alternate_allele', 'key': 'alt'}], paths['alternate_allele'])
--- sofia_/test/test_parser/test_resource_parser.py
+++ sofia_/test/test_parser/test_resource_parser.py
@@ -11,44 +11,44 @@ class TestResourceParser(unittest.TestCase):
     def setUp(self):
         self.hndl, self.name = tempfile.mkstemp()
         os.close(self.hndl)
-        self.parser = ResourceParser()
+        self.parser = ResourceParser({'': 'test'})
 
     def test_parse_resource_basic(self):
         resource = self.parser.parse_resource(self.name)
 
-        self.assertEqual(self.name, resource.fname)
-        self.assertEqual(os.path.basename(self.name), resource.name)
+        self.assertEqual(self.name, resource.attr['filename'])
+        self.assertEqual('test', resource.name)
 
     def test_parse_resource_name(self):
         resource_string = '{}:test'.format(self.name)
         resource = self.parser.parse_resource(resource_string)
 
-        self.assertEqual(self.name, resource.fname)
+        self.assertEqual(self.name, resource.attr['filename'])
         self.assertEqual('test', resource.name)
 
     def test_parse_resource_attribute(self):
         resource_string = '{}:x=x'.format(self.name)
         resource = self.parser.parse_resource(resource_string)
 
-        self.assertEqual(self.name, resource.fname)
-        self.assertEqual(os.path.basename(self.name), resource.name)
-        self.assertEqual({'x': ['x']}, resource.attr)
+        self.assertEqual(self.name, resource.attr['filename'])
+        self.assertEqual('test', resource.name)
+        self.assertEqual({'x': 'x', 'filename': self.name}, resource.attr)
 
     def test_parse_resource_name_attribute(self):
         resource_string = '{}:test:x=x'.format(self.name)
         resource = self.parser.parse_resource(resource_string)
 
-        self.assertEqual(self.name, resource.fname)
+        self.assertEqual(self.name, resource.attr['filename'])
         self.assertEqual('test', resource.name)
-        self.assertEqual({'x': ['x']}, resource.attr)
+        self.assertEqual({'x': 'x', 'filename': self.name}, resource.attr)
 
     def test_parse_resource_attribute_name(self):
         resource_string = '{}:x=x:test'.format(self.name)
         resource = self.parser.parse_resource(resource_string)
 
-        self.assertEqual(self.name, resource.fname)
+        self.assertEqual(self.name, resource.attr['filename'])
         self.assertEqual('test', resource.name)
-        self.assertEqual({'x': ['x']}, resource.attr)
+        self.assertEqual({'x': 'x', 'filename': self.name}, resource.attr)
 
 
 if __name__ == '__main__':
