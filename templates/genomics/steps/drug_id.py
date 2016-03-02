7e459f2dd3e10804da6523eadb3caab6897668ea
Liam Childs <liam_childs@hotmail.com>
2016-03-02 09:51:07 +0100

    added Genomics of Drug Sensitivity in Cancer resource

--- templates/genomics/steps/drug_id.py
+++ templates/genomics/steps/drug_id.py
@@ -7,14 +7,14 @@ class DrugBankMap(Resource):
     OUT = ['drug_bank_map']
 
     def get_interface(self, filename):
-        res = {}
+        interface = {}
         fhndl = open(filename)
         fhndl.next()
         for line in fhndl:
             parts = line.rstrip('\r\n').split(',')
-            res[parts[0]] = [part.strip() for part in parts[12].split(';')]
+            interface[parts[0]] = [part.strip() for part in parts[12].split(';')]
         fhndl.close()
-        return res
+        return interface
 
 
 class GetDrugIdFromDrugBank(Step):
@@ -24,3 +24,30 @@ class GetDrugIdFromDrugBank(Step):
 
     def calculate(self, drug_bank_map, transcript_id):
         return drug_bank_map[transcript_id] if transcript_id in drug_bank_map else None
+
+
+class GenomicsOfDrugSensitivityInCancerByGene(Resource):
+
+    FORMAT = 'gdsc_map'
+    OUT = ['gdsc_by_gene']
+
+    def get_interface(self, filename):
+        interface = {}
+        fhndl = open(filename)
+        fhndl.next()
+        for line in fhndl:
+            parts = line.rstrip('\r\n').split(',')
+            genes = parts[0].split('///')
+            for gene in genes:
+                interface[gene.strip()] = parts[2]
+        fhndl.close()
+        return interface
+
+
+class GetDrugIdFromGDSC(Step):
+
+    IN = ['gdsc_by_gene', 'gene_id']
+    OUT = ['drug_id']
+
+    def calculate(self, gdsc_by_gene, gene_id):
+        return gdsc_by_gene[gene_id] if gene_id in gdsc_by_gene else None
