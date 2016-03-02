29bb30d9dd83522db05a1d204914c84fca4111eb
Liam Childs <liam_childs@hotmail.com>
2016-03-02 09:49:32 +0100

    added tabix indexed maf file

--- lhc-python/lhc/io/maf_/iterator.py
+++ lhc-python/lhc/io/maf_/iterator.py
@@ -3,15 +3,17 @@ import gzip
 
 from collections import namedtuple
 
-MafLine = namedtuple('MafLine', ('hugo_symbol', 'entrez_gene_id', 'center', 'build', 'chromosome', 'start_position',
-                                 'end_position', 'strand', 'variant_classification', 'reference_allele',
+MafLine = namedtuple('MafLine', ('hugo_symbol', 'entrez_gene_id', 'center', 'ncbi_build', 'chromosome', 'start_position',
+                                 'end_position', 'strand', 'variant_classification', 'variant_type', 'reference_allele',
                                  'tumour_seq_allele1', 'tumour_seq_allele2', 'dnsnp_rs', 'dbsnp_val_status',
                                  'tumour_sample_barcode', 'matched_norm_sample_barcode', 'match_norm_seq_allele1',
                                  'match_norm_seq_allele2', 'tumour_validation_allele1', 'tumour_validation_allele2',
                                  'match_norm_validation_allele1', 'match_norm_validation_allele2',
                                  'verification_status', 'validation_status', 'mutation_status', 'sequencing_phase',
                                  'sequence_source', 'validation_method', 'score', 'bam_file', 'sequencer',
-                                 'tumour_sample_uuid', 'matched_norm_sample_uuid'))
+                                 'tumour_sample_uuid', 'matched_norm_sample_uuid', 'cosmic_codon', 'cosmic_gene',
+                                 'transcript_id', 'exon', 'chrom_change', 'aa_change', 'genome_plus_minus_10_bp',
+                                 'drug_target', 'ttot_cov', 'tvar_cov', 'ntot_cov', 'nvar_cov', 'dbSNPPopFreq'))
 
 
 class MafIterator(object):
