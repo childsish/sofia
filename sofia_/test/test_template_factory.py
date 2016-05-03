884e7773867dcbf7a048451f3e24241100562779
Liam Childs <liam_childs@hotmail.com>
2016-05-03 10:56:21 +0200

    fixed template factory unit test

--- sofia_/test/test_template_factory.py
+++ sofia_/test/test_template_factory.py
@@ -11,7 +11,7 @@ class TestTemplateFactory(unittest.TestCase):
         factory = TemplateFactory(path)
 
         self.assertTrue(all(inherits_from(step, 'Step') for step in factory.steps))
-        self.assertEqual(22, sum(inherits_from(step, 'Resource') for step in factory.steps))
+        self.assertEqual(20, sum(inherits_from(step, 'Resource') for step in factory.steps))
 
 
 if __name__ == '__main__':
