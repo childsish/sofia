9b323849440d0dda5d1611467b1ef80d81050f54
Liam Childs <liam_childs@hotmail.com>
2016-05-31 15:54:38 +0200

    added attribute resolver

---
+++ sofia/resolvers/__init__.py
@@ -0,0 +1,3 @@
+from attribute_resolver import AttributeResolver
+from entity_resolver import EntityResolver
+from step_resolver import StepResolver
---
+++ sofia/resolvers/attribute_resolver.py
@@ -0,0 +1,21 @@
+from operator import or_
+
+
+class AttributeResolver(object):
+
+    ATTRIBUTE = ''
+
+    def __init__(self, attribute, entity_graph, step, id_maps):
+        self.attribute = attribute
+        self.entity_graph = entity_graph
+        self.step = step
+        self.id_maps = id_maps
+
+    def resolve_in(self, ins):
+        return ins
+
+    def resolve_out(self, ins):
+        values = reduce(or_, ins.itervalues())
+        return {
+            entity: values for entity in self.step.outs
+        }
