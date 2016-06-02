53fe8f164874e1178506eb8b4b1511490b7d600a
Liam Childs <liam_childs@hotmail.com>
2016-06-02 14:11:56 +0200

    removed unused code

--- sofia/workflow_iterator.py
+++
@@ -1,6 +0,0 @@
-class WorkflowIterator(object):
-    def __init__(self, workflow):
-        self.workflow = workflow
-
-    def next(self):
-        return [head.next() for head in self.workflow.heads]
