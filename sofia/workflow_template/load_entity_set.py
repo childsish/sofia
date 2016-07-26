81c6f28185d216f31d60d0dcf588a346bdb91faa
Liam Childs <liam_childs@hotmail.com>
2016-07-26 22:01:29 +0200

    fixed entity set defaults

--- sofia/workflow_template/load_entity_set.py
+++ sofia/workflow_template/load_entity_set.py
@@ -1,14 +1,21 @@
 import json
+import os
 
 from entity_set import EntitySet
 from sofia.workflow_template.entity_definition_parser import EntityDefinitionParser
 
 
 def load_entity_set(filename):
+    entities = EntitySet()
+    extensions = {}
+    provided_entities = []
+
+    if not os.path.exists(filename):
+        return entities, EntityDefinitionParser(extensions), provided_entities
+
     with open(filename) as fileobj:
         entity_definitions = json.load(fileobj)
 
-    entities = EntitySet()
     for definition in entity_definitions:
         entities.register_entity(definition['name'],
                                  definition.get('description', None),
@@ -16,7 +23,6 @@ def load_entity_set(filename):
                                  definition.get('is_a', None),
                                  definition.get('attributes', None))
 
-    extensions = {}
     for definition in entity_definitions:
         for extension in definition.get('extensions', []):
             if extension in extensions and extensions[extension] != definition['name']:
@@ -25,8 +31,7 @@ def load_entity_set(filename):
             extensions[extension] = definition['name']
     parser = EntityDefinitionParser(extensions)
 
-    provided_entities = []
     for definition in entity_definitions:
         for provided in definition.get('provided', []):
-            provided_entities.append(parser.parse_provided_entity(provided.split()))
+            provided_entities.append(parser.parse_provided_entity(provided.split(), definition['name']))
     return entities, parser, provided_entities
