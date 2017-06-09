3e64d37fa753d0ea3fcb1f4dd1d5f96a037966f9
Liam Childs <Liam Childs>
2017-06-09 10:46:10 +0200

    improved error reporting for sync attribute resolver

--- templates/genomics/attributes/sync.py
+++ templates/genomics/attributes/sync.py
@@ -1,14 +1,21 @@
 from sofia.resolvers import AttributeResolver
 
 
-class ChromosomeIdResolver(AttributeResolver):
+class SyncResolver(AttributeResolver):
 
     ATTRIBUTE = 'sync'
 
     def resolve_out(self, ins):
+        """
+        Determine which stream the output is synchronised with. If the incoming streams have different sync values, then
+         it is unknown what synchronisation the outgoing stream should have.  
+        :param ins: dictionary of the incoming streams' sync values
+        :return: 
+        """
         values = set()
         for value in ins.values():
             values.update(value)
         if len(values) > 1:
-            raise ValueError('Unable to resolve sync stream')
+            msg = 'Unable to resolve sync stream. Consider adding a custom resolver to {}.'
+            raise ValueError(msg.format(self.step.name))
         return {key: values for key in self.step.outs}
