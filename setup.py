93485a2620ccd67670d80e6bea99988854ff91b6
Liam Childs <Liam Childs>
2017-06-14 13:31:43 +0200

    fixed static data location

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author='Liam H. Childs',
     author_email='liam.h.childs@gmail.com',
     packages=find_packages(exclude=['tests']),
-    package_data={'templates': ['genomics/*.json', 'genomics/entities/*']},
+    package_data={'templates': ['genomics/*.json', 'genomics/data/*']},
     url='https://github.com/childsish/sofia',
     license='LICENSE.txt',
     description='Software for the Flexible Integration of Annotation',
